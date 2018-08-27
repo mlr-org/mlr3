d9f50d29df56e5ba8152e723c6e7900b4142bfe1
Michel Lang <michellang@gmail.com>
2018-08-27 21:45:17 +0200

    cleanup holdout

--- R/mlr_resamplings_holdout.R
+++ R/mlr_resamplings_holdout.R
@@ -1,8 +1,36 @@
 #' @include Resampling.R
 #' @include mlr_resamplings_subsampling.R
-local({
-  rr = ResamplingSubsampling$new()
-  rr$id = "holdout"
-  rr$repeats = 1L
-  mlr_resamplings$add(rr)
-})
+ResamplingHoldout = R6Class("ResamplingHoldout", inherit = Resampling,
+  public = list(
+    id = "holdout",
+    ratio = 0.67,
+    iters = 1L,
+    instantiate = function(task, ...) {
+      # inner function so we can easily implement blocking here
+      # -> replace ids with unique values of blocking variable
+      # -> join ids using blocks
+      holdout = function(ids, ratio) {
+        ii = sample(ids, ratio * length(ids))
+        list(train = ii, test = setdiff(ids, ii))
+      }
+
+      assert_task(task)
+      private$instance = holdout(task$row_ids(), assert_number(self$ratio, lower = 0))
+      self
+    },
+
+    train_set = function(i) {
+      i = assert_resampling_index(self, i)
+      private$instance$train
+    },
+
+    test_set = function(i) {
+      i = assert_resampling_index(self, i)
+      private$instance$test
+    }
+  )
+)
+
+mlr_resamplings$add(
+  ResamplingHoldout$new()
+)
