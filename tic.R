90a9f9d4f3909ffeb576949f9c6fb174c4d7903d
pat-s <patrick.schratz@gmail.com>
2018-10-01 17:58:57 +0200

    devtools::install

--- tic.R
+++ tic.R
@@ -2,6 +2,7 @@
 
 get_stage("script") %>%
   add_code_step(devtools::build(manual = TRUE)) %>%
+  add_code_step(devtools::install(quick = TRUE)) %>%
   add_code_step(rcmdcheck::rcmdcheck(path = "../mlr3_0.1.0-9000.tar.gz", args = "--as-cran",
                                      error_on = "error"))
 
