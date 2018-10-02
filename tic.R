a73e862f60bc873351e83cbfe1c92edeae1dfb7e
pat-s <patrick.schratz@gmail.com>
2018-10-02 12:54:16 +0200

    fix error handling

--- tic.R
+++ tic.R
@@ -1,7 +1,8 @@
 # add_package_checks()
 
 get_stage("script") %>%
-  add_step(step_rcmdcheck(args = "--as-cran", error_on = "error"))
+  add_step(step_rcmdcheck(args = "--as-cran", warnings_are_errors = FALSE,
+                          notes_are_errors = FALSE))
   # add_code_step(devtools::build(manual = TRUE)) %>%
   # add_code_step(devtools::install(quick = TRUE)) %>%
   # add_code_step(rcmdcheck::rcmdcheck(path = "../mlr3_0.1.0-9000.tar.gz", args = "--as-cran",
