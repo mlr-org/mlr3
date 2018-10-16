995fed5c5cef2a0d2668f55dcdc7e30cad8a976e
Michel Lang <michellang@gmail.com>
2018-10-16 15:52:28 +0200

    speedup index helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -9,13 +9,13 @@ if (getOption("mlr3.debug", FALSE)) {
 }
 
 `[[.R6` = function(x, i, ...) {
-  if (i %in% names(x))
+  if (exists(i, envir = x, inherits = FALSE))
     return(get(i, envir = x))
   stop("R6 class ", paste0(class(x), collapse = "/") ," does not have slot '", i, "'!")
 }
 
 `$.R6` = function(x, name) {
-  if (name %in% names(x))
+  if (exists(name, envir = x, inherits = FALSE))
     return(get(name, envir = x))
   stop("R6 class ", paste0(class(x), collapse = "/") ," does not have slot '", name, "'!")
 }
