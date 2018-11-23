d707c103e2abd868cbc57746a96b38776b81d358
Michel Lang <michellang@gmail.com>
2018-11-23 11:51:48 +0100

    helper cleanup

--- R/helper_data.table.R
+++
@@ -1,7 +0,0 @@
-# updating join:
-# replaces values in x with values in y
-ujoin = function(x, y, key) {
-  cn = setdiff(intersect(names(x), names(y)), key)
-  expr = parse(text = paste0("`:=`(", paste0(sprintf("%1$s=i.%1$s", cn), collapse = ","), ")"))
-  x[y, eval(expr), on = key]
-}
