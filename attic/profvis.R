6a95803514af7aafa0573dfe5fb1cc6074ba1d9b
Michel Lang <michellang@gmail.com>
2018-11-26 13:32:32 +0100

    update attic profile script

--- attic/profvis.R
+++ attic/profvis.R
@@ -4,10 +4,10 @@ devtools::load_all()
 
 
 tasks = mlr_tasks$mget(c("iris", "sonar"))
-learners = mlr_learners$mget(c("classif.dummy"))
+learners = mlr_learners$mget(c("classif.featureless"))
 resamplings = mlr_resamplings$mget("subsampling")
 measures = mlr_measures$mget(c("acc", "time_train"))
 
 profvis::profvis(
-  benchmark(tasks, learners, resamplings, measures, exec_control(verbose = FALSE, use_future = FALSE))
+  benchmark(tasks, learners, resamplings, measures, mlr_control(verbose = FALSE, use_future = FALSE))
 )
