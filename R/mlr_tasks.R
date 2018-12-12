5aa8057df5edc95a90fb9c43562ed07e28f8bfed
Michel Lang <michellang@gmail.com>
2018-12-13 00:08:53 +0100

    document tasks

--- R/mlr_tasks.R
+++ R/mlr_tasks.R
@@ -2,6 +2,7 @@
 #'
 #' @description
 #' A simple [Dictionary] storing objects of class [Task].
+#' Each task has an associated help page, see `mlr_tasks_[id]`.
 #'
 #' @section Usage:
 #' See [Dictionary].
@@ -62,36 +63,74 @@ load_dataset = function(id, package, keep.rownames = FALSE) {
   ee[[id]]
 }
 
+
+#' @title Iris classification task
+#' @name mlr_tasks_iris
+#' @description
+#' A classification task for the popular [datasets::iris] data set.
 mlr_tasks$add("iris", function() {
   b = as_data_backend(load_dataset("iris", "datasets"))
   TaskClassif$new("iris", b, target = "Species")
 })
 
+#' @title Sonar classification task
+#' @name mlr_tasks_sonar
+#' @description
+#' A classification task for the [mlbench::Sonar] data set.
+#' Positive class is set to "M" (Mine).
 mlr_tasks$add("sonar",  function() {
   b = as_data_backend(load_dataset("Sonar", "mlbench"))
   TaskClassif$new("sonar", b, target = "Class", positive = "M")
 })
 
+#' @title Boston Housing regression task
+#' @name mlr_tasks_bh
+#' @description
+#' A regression task for the [mlbench::BostonHousing2] data set.
 mlr_tasks$add("bh",  function() {
   b = as_data_backend(load_dataset("BostonHousing2", "mlbench"))
   TaskRegr$new("boston_housing", b, target = "medv")
 })
 
+#' @title Pima Indian Diabetes classification task
+#' @name mlr_tasks_pima
+#' @description
+#' A classification task for the [mlbench::PimaIndiansDiabetes2] data set.
+#' Positive class is set to "pos".
 mlr_tasks$add("pima", function() {
   b = as_data_backend(load_dataset("PimaIndiansDiabetes2", "mlbench"))
   TaskClassif$new("pima_indians", b, target = "diabetes", positive = "pos")
 })
 
+#' @title Zoo classification task
+#' @name mlr_tasks_zoo
+#' @description
+#' A classification task for the [mlbench::Zoo] data set.
 mlr_tasks$add("zoo", function() {
   b = as_data_backend(load_dataset("Zoo", "mlbench", keep.rownames = TRUE))
   TaskClassif$new("zoo", b, target = "type")
 })
 
+#' @title Spam classification task
+#' @name mlr_tasks_spam
+#' @description
+#' A classification task for the [kernlab::spam] data set.
+#' Positive class is set to "spam".
 mlr_tasks$add("spam", function() {
   b = as_data_backend(load_dataset("spam", "kernlab"))
   TaskClassif$new("spam", b, target = "type", positive = "spam")
 })
 
+#' @title Titanic classification task
+#' @name mlr_tasks_titanic
+#' @description
+#' A classification task for the [titanic::titanic] data set.
+#'
+#' The following preprocessing steps are already performed:
+#' * Training and test data has been merged. In the test data, the target variable "Survived" is NA.
+#' * Columns `Survived` and `Sex` have been converted to a factor.
+#' * Columns `Embarked` and `Cabin` have been converted to a factor and empty string have been replaced with NA.
+#' * The positive class is set to "1" (Survived).
 mlr_tasks$add("titanic", function() {
   data = rbindlist(list(load_dataset("titanic_train", package = "titanic"), load_dataset("titanic_test", package = "titanic")), fill = TRUE)
   data$Survived = factor(data$Survived, levels = c("0", "1"))
