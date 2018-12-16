231ac7a87f2cf40efc5c45bdc8896d6e3d0a7321
Michel Lang <michellang@gmail.com>
2018-12-16 22:49:31 +0100

    Fix docs on hashing

--- R/Task.R
+++ R/Task.R
@@ -130,7 +130,7 @@
 #' * `$overwrite()` overwrite the data in the [DataBackend] with data provided as [data.table::data.table()].
 #'   The row ids must be provided as column in `data` (with column name matching the primary key name of the [DataBackend]).
 #'
-#' * `$hash` (`character(1)`) stores a checksum calculated on the `id`, `row_roles` and `col_info`.
+#' * `$hash` (`character(1)`) stores a checksum calculated on the `id`, `row_roles` and `col_roles`.
 #'
 #' @section Task mutators:
 #' The methods `filter()`, `select()`, `rbind()`, `cbind()`, and `overwrite()` change the task in-place,
