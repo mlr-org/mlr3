d5c7b9e713dd599ed6747c5100491d9d87d141b6
Michel Lang <michellang@gmail.com>
2018-12-17 07:23:53 +0100

    moved str_indent to mlr3misc

--- R/helper_print.R
+++ R/helper_print.R
@@ -1,8 +1,3 @@
-str_indent = function(str, initial, n = 100L) {
-  str = if (length(str) == 0L) "-" else paste0(head(str, n), collapse = ", ")
-  strwrap(str, initial = initial, exdent = 2L)
-}
-
 str_r6_interface = function(x) {
   categorize = function(name) {
     if (bindingIsActive(name, x))
