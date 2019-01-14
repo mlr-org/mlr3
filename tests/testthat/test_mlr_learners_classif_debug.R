790fd65fa0ebc67362c118349cc6a168175d1bd8
Michel Lang <michellang@gmail.com>
2019-01-14 11:05:52 +0100

    fix debug learner test

--- tests/testthat/test_mlr_learners_classif_debug.R
+++ tests/testthat/test_mlr_learners_classif_debug.R
@@ -1,7 +1,6 @@
 context("mlr_learners_regr_featureless")
 
 test_that("Simple training/predict", {
-
   task = mlr_tasks$get("iris")
   learner = mlr_learners$get("classif.debug")
   expect_learner(learner, task)
@@ -18,18 +17,14 @@ test_that("Simple training/predict", {
   e$score()
 
   e$learner$param_vals$save_tasks = TRUE
-
   e$train(row_ids = 1:10)
   e$predict(row_ids = 11:20)
-  itrain = task$clone(deep = TRUE)
-  itrain$hash
-  itrain$filter(1:10)
-  ipredict = task$clone(deep = TRUE)
-  ipredict$hash  # FIXME: when issue 130 is fixed this line can go
-  ipredict$filter(11:20)
-  ipredict$hash  # FIXME: when issue 130 is fixed this line can go
-  expect_equal(e$model, list(itrain, ipredict))
+  model = e$model
 
+  itrain = task$clone(TRUE)$filter(1:10)
+  ipredict = task$clone(TRUE)$filter(11:20)
+
+  expect_equal(hashes(model), hashes(list(itrain, ipredict)))
 })
 
 test_that("autotest", {
