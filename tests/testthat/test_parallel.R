f473804cc92dc9b945192ed43d94ec6e5ee05a0b
Michel Lang <michellang@gmail.com>
2019-01-17 14:09:36 +0100

    added a test for parallel benchmarking

--- tests/testthat/test_parallel.R
+++ tests/testthat/test_parallel.R
@@ -16,3 +16,19 @@ test_that("parallel resample", {
   expect_resample_result(rr)
   expect_false(any(rr$errors))
 })
+
+test_that("parallel benchmark", {
+  skip_if_not_installed("future")
+  skip_if_not_installed("future.callr")
+  future::plan(future.callr::callr)
+  expect_true(future_remote())
+
+  task = mlr_tasks$get("iris")
+  learner = mlr_learners$get("classif.rpart")
+  resampling = mlr_resamplings$get("cv")
+  resampling$param_vals = list(folds = 3)
+
+  bmr = benchmark(expand_grid(list(task), list(learner), list(resampling)))
+  expect_benchmark_result(bmr)
+  # expect_false(any(rr$errors))
+})
