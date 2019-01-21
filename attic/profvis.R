7a10b65d87736f093ed8bb1ebf259a95216849a3
Michel Lang <michellang@gmail.com>
2019-01-21 13:12:15 +0100

    fix profvis script

--- attic/profvis.R
+++ attic/profvis.R
@@ -7,7 +7,7 @@ library(logger)
 tasks = mlr_tasks$mget(c("iris", "sonar"))
 learners = mlr_learners$mget(c("classif.featureless"))
 resamplings = mlr_resamplings$mget("subsampling")
-measures = mlr_measures$mget(c("acc", "time_train"))
+measures = mlr_measures$mget(c("classif.acc", "time_train"))
 
 log_threshold(ERROR, namespace = "mlr3")
 profvis::profvis(
