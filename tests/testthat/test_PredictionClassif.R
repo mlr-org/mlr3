b482d3b3f7aa1cd12347b5242489ff33b23c3cfa
Michel Lang <michellang@gmail.com>
2019-02-27 21:51:28 +0100

    Thresholds for PredictionClassif (#189)

--- tests/testthat/test_PredictionClassif.R
+++ tests/testthat/test_PredictionClassif.R
@@ -25,3 +25,28 @@ test_that("partial results", {
   expect_prediction(p)
   expect_prediction_classif(p)
 })
+
+test_that("setting threshold", {
+  task = mlr_tasks$get("sonar")
+  lrn = mlr_learners$get("classif.rpart", predict_type = "prob")
+  e = Experiment$new(task, lrn)$train()$predict()
+  p = e$prediction
+  expect_factor(p$response, levels = task$all_classes)
+  expect_equal(as.character(p$response), colnames(p$prob)[max.col(p$prob)])
+
+  response_before = p$response
+  p$threshold = 0.5
+  expect_factor(p$response, levels = task$all_classes, any.missing = FALSE)
+  expect_equal(p$response, response_before)
+  expect_lt(e$score()$performance, 0.25)
+
+  p$threshold = 0
+  expect_factor(p$response, levels = task$all_classes, any.missing = FALSE)
+  expect_true(all(as.character(p$response) == task$positive))
+  expect_gt(e$score()$performance, 0.25)
+
+  p$threshold = 1
+  expect_factor(p$response, levels = task$all_classes, any.missing = FALSE)
+  expect_true(all(as.character(p$response) == task$negative|| p$prob[, task$negative] == 0))
+  expect_gt(e$score()$performance, 0.25)
+})
