e1793028f1c62f2e82d97c1923685ca396b28395
Coorsaa <stefan.coors@gmx.net>
2019-03-18 17:11:37 +0100

    tests for multiclass thresholding

--- tests/testthat/test_PredictionClassif.R
+++ tests/testthat/test_PredictionClassif.R
@@ -27,6 +27,7 @@ test_that("partial results", {
 })
 
 test_that("setting threshold", {
+  # binary classification
   task = mlr_tasks$get("sonar")
   lrn = mlr_learners$get("classif.rpart", predict_type = "prob")
   e = Experiment$new(task, lrn)$train()$predict()
@@ -49,4 +50,33 @@ test_that("setting threshold", {
   expect_factor(p$response, levels = task$class_names, any.missing = FALSE)
   expect_true(all(as.character(p$response) == task$negative|| p$prob[, task$negative] == 0))
   expect_gt(e$score()$performance, 0.25)
+
+  # multiclass classification
+  task = mlr_tasks$get("iris")
+  lrn = mlr_learners$get("classif.rpart", predict_type = "prob")
+  e = Experiment$new(task, lrn)$train()$predict()
+  p = e$prediction
+  expect_factor(p$response, levels = task$class_names)
+  expect_equal(as.character(p$response), colnames(p$prob)[max.col(p$prob)])
+
+  response_before = p$response
+
+  expect_error({p$threshold = c(0.5, 0.5)})
+  expect_error({p$threshold = c(0.1, 0.5, 0.5)})
+
+  p$threshold = c(0, 0.5, 0.5)
+  expect_factor(p$response, levels = task$class_names, any.missing = FALSE)
+  expect_equal(levels(droplevels(p$response)), task$class_names[1])
+  expect_gt(e$score()$performance, .66)
+
+  p$threshold = c(0.5, 0, 0.5)
+  expect_factor(p$response, levels = task$class_names, any.missing = FALSE)
+  expect_equal(levels(droplevels(p$response)), task$class_names[2])
+  expect_gt(e$score()$performance, 0.66)
+
+  p$threshold = c(0.5, 0.5, 0)
+  expect_factor(p$response, levels = task$class_names, any.missing = FALSE)
+  expect_equal(levels(droplevels(p$response)), task$class_names[3])
+  expect_gt(e$score()$performance, 0.66)
+
 })
