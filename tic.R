7584bb1710e9ed7c960e6e4c88446e7a3c363cb1
pat-s <patrick.schratz@gmail.com>
2019-04-14 14:34:29 +0200

    test

--- tic.R
+++ tic.R
@@ -1,36 +1,28 @@
-if (ci_has_env("TravisCI")) {
+if (inherits(ci(), "TravisCI")) {
 
   do_package_checks(args = "--as-cran", error_on = "error")
 
 }
 
-if (ci_has_env("AppVeyorCI")) {
+if (inherits(ci(), "AppVeyorCI")) {
 
   do_package_checks(
     args = c("--as-cran", "--no-manual", "--no-vignettes", "--no-build-vignettes"),
-    build_args = "--no-build-vignettes", error_on = "error")
+    build_args = c("--no-build-vignettes"),
+    error_on = "error"
+  )
+
 }
 
-if (ci_has_env("id_rsa")) {
+if (ci_can_push()) {
 
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
+  do_pkgdown(commit_paths = "docs")
+}
+
+# only deploy man files in master branch
+if (ci_get_branch() == "master" && ci_is_env("TRAVIS_EVENT_TYPE", "cron")) {
 
   get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(commit_paths = "docs"))
-}
-
-# only deploy man files on Travis on non-cron builds
-# only run codecov on Travis
-if (ci_has_env("TravisCI") && !ci_is_env("TRAVIS_EVENT_TYPE", "cron")) {
-
-  if (ci()$get_branch() == "master") {
-    get_stage("deploy") %>%
-      add_code_step(devtools::document()) %>%
-      add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
-  }
-
-  get_stage("after_deploy") %>%
-    add_code_step(covr::codecov(quiet = FALSE))
+    add_code_step(devtools::document()) %>%
+    add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
 }
