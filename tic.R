5c5bfea5a7f1e0def3c28346e21b211e65b49641
Patrick Schratz <patrick.schratz@gmail.com>
2019-05-07 12:56:47 +0200

    Update tic and remove vignettes (#215)

--- tic.R
+++ tic.R
@@ -1,28 +1,3 @@
-if (inherits(ci(), "TravisCI")) {
+do_package_checks(error_on = "error")
 
-  do_package_checks(args = "--as-cran", error_on = "error")
-
-}
-
-if (inherits(ci(), "AppVeyorCI")) {
-
-  do_package_checks(
-    args = c("--as-cran", "--no-manual", "--no-vignettes", "--no-build-vignettes"),
-    build_args = c("--no-build-vignettes"),
-    error_on = "error"
-  )
-
-}
-
-if (ci_can_push()) {
-
-  do_pkgdown(commit_paths = "docs")
-}
-
-# only deploy man files in master branch
-if (ci_get_branch() == "master" && ci_is_env("TRAVIS_EVENT_TYPE", "cron")) {
-
-  get_stage("deploy") %>%
-    add_code_step(devtools::document()) %>%
-    add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
-}
+do_pkgdown()
