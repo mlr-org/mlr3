cd2bba327cee4f202c56a33a50f020c6d6854ac0
pat-s <patrick.schratz@gmail.com>
2019-05-07 11:26:11 +0200

    remove man file deployment

--- tic.R
+++ tic.R
@@ -1,11 +1,3 @@
 do_package_checks()
 
 do_pkgdown()
-
-# only deploy man files in master branch
-if (ci_get_branch() == "master" && ci_is_env("TRAVIS_EVENT_TYPE", "cron")) {
-
-  get_stage("deploy") %>%
-    add_code_step(devtools::document()) %>%
-    add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
-}
