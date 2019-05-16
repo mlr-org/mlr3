098dcefcb77851dcc39f1e38e512325d6080f8dd
Michel Lang <michellang@gmail.com>
2019-05-16 13:56:51 +0200

    added (commented) test for mem usage

--- tests/testthat/test_parallel.R
+++ tests/testthat/test_parallel.R
@@ -35,3 +35,28 @@ test_that("parallel benchmark", {
     expect_benchmark_result(bmr)
   })
 })
+
+
+# test_that("parallel resample / duplication", {
+#   skip_if_not_installed("future")
+#   skip_if_not_installed("future.callr")
+
+#   task = mlr_tasks$get("iris")
+#   learner = mlr_learners$get("classif.rpart")
+#   resampling = mlr_resamplings$get("cv3")$instantiate(task)
+#   ctrl = mlr_control(store_model = FALSE)
+
+#   rr1 = with_future("sequential", {
+#     expect_false(use_future())
+#     resample(task, learner, resampling, ctrl = ctrl)
+#   })
+
+#   rr2 = with_future("multiprocess", {
+#     expect_true(use_future())
+#     resample(task, learner, resampling, ctrl = ctrl)
+#   })
+
+#   pryr::object_size(learner)
+#   pryr::object_size(rr1$data$learner[[1]])
+#   pryr::object_size(rr2$data$learner[[1]])
+# })
