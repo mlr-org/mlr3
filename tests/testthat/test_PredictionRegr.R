74f56067701b39e243c911cbb32b7734fe13c002
Michel Lang <michellang@gmail.com>
2019-06-24 17:23:23 +0200

    repair test

--- tests/testthat/test_PredictionRegr.R
+++ tests/testthat/test_PredictionRegr.R
@@ -19,13 +19,13 @@ test_that("Internally constructed Prediction", {
 })
 
 
-test_that("rbind", {
+test_that("c", {
   task = mlr_tasks$get("boston_housing")
   lrn = mlr_learners$get("regr.featureless")
   lrn$predict_type = "se"
   rr = resample(task, lrn, "cv3")
 
-  pred = do.call(rbind, map(rr$experiments(), "prediction"))
+  pred = do.call(c, map(rr$experiments(), "prediction"))
   expect_prediction(pred)
   expect_prediction_regr(pred)
 
@@ -33,12 +33,12 @@ test_that("rbind", {
   expect_data_table(dt, nrow = task$nrow, ncol = 4L, any.missing = FALSE)
 })
 
-test_that("rbind drops se (#250)", {
+test_that("c drops se (#250)", {
   task = mlr_tasks$get("boston_housing")
   lrn = mlr_learners$get("regr.featureless")
   rr = resample(task, lrn, "cv3")
 
-  pred = do.call(rbind, map(rr$experiments(), "prediction"))
+  pred = do.call(c, map(rr$experiments(), "prediction"))
   expect_null(pred$se)
   expect_false("se" %in% names(as.data.table(pred)))
 })
