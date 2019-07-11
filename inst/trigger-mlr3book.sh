b63e09f791c7b1d5598b4871f6c161f430c86905
pat-s <patrick.schratz@gmail.com>
2019-07-11 17:55:39 +0200

    update trigger script

--- inst/trigger-mlr3book.sh
+++ inst/trigger-mlr3book.sh
@@ -3,4 +3,6 @@ if [[ ($TRAVIS_BRANCH == master) &&
       ($TRAVIS_PULL_REQUEST == false) ]] ; then
   curl -LO --retry 3 https://raw.github.com/mernst/plume-lib/master/bin/trigger-travis.sh
   sh trigger-travis.sh mlr-org mlr3book $TRAVIS_ACCESS_TOKEN "trigger from mlr3"
+else
+  echo "Not triggering a mlr3book deployment"
 fi
