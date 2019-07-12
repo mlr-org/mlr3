6753e519e614cb5f7244446192dba31c42597f5e
Patrick Schratz <patrick.schratz@gmail.com>
2019-07-12 14:30:38 +0200

    Trigger mlr3book CI run on every successful master build (#275)
    * try trigger
    
    * fix tic.R
    
    * really fix
    
    * now?
    
    * use ' instead of "
    
    * update trigger script
    
    * try here package
    
    * next try
    
    * ...
    
    * test

--- tic.R
+++ tic.R
@@ -3,3 +3,6 @@ do_package_checks(error_on = "error")
 if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown(orphan = TRUE)
 }
+
+get_stage("after_success") %>%
+  add_code_step(system("bash ./inst/trigger-mlr3book.sh"))
