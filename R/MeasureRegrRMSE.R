c346cc122beace718a254c5e08408acc1cc759e9
pfistfl <pfistererf@googlemail.com>
2019-07-14 20:30:28 +0200

    Add RMSE

---
+++ R/MeasureRegrRMSE.R
@@ -0,0 +1,27 @@
+#' @title Root Mean Squared Error Regression Measure
+#'
+#' @aliases mlr_measures_regr.rmse
+#' @format [R6::R6Class] inheriting from [MeasureClassif].
+#' @include MeasureRegr.R
+#'
+#' @description
+#' Calls [Metrics::rmse()].
+#'
+#' @export
+MeasureRegrMSE = R6Class("MeasureRegrRMSE",
+  inherit = MeasureRegr,
+  public = list(
+    initialize = function() {
+      super$initialize(
+        id = "regr.rmse",
+        range = c(0, Inf),
+        minimize = TRUE,
+        packages = "Metrics"
+      )
+    },
+
+    score_internal = function(prediction, ...) {
+      Metrics::rmse(actual = prediction$truth, predicted = prediction$response)
+    }
+  )
+)
