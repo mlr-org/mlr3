29a7188b7d0fad0e2a7c17e4e650fb3a93c51382
Michel Lang <michellang@gmail.com>
2019-07-24 11:23:08 +0200

    ...

--- R/worker.R
+++ R/worker.R
@@ -1,24 +1,55 @@
-learner_train = function(learner, task, row_ids = NULL, ctrl = mlr_control()) {
-  # This wrapper calls learner$train, and additionally performs some basic
-  # checks that the training was successful.
-  # Exceptions here are possibly encapsulated, so that they get captured
-  # and turned into log messages.
-  wrapper = function(learner, task) {
-    tryCatch(
-      { model = learner$train_internal(task = task) },
-      error = function(e) {
-        e$message = sprintf("Learner '%s' on task '%s' failed to fit: %s", learner$id, task$id, e$message)
-        stop(e)
-      }
-    )
-
-    if (is.null(model)) {
-      stopf("Learner '%s' on task '%s' returned NULL during train_internal()", learner$id, task$id)
+# This wrapper calls learner$train, and additionally performs some basic
+# checks that the training was successful.
+# Exceptions here are possibly encapsulated, so that they get captured
+# and turned into log messages.
+train_wrapper = function(learner, task) {
+  tryCatch(
+    { model = learner$train_internal(task = task) },
+    error = function(e) {
+      e$message = sprintf("Learner '%s' on task '%s' failed to fit: %s", learner$id, task$id, e$message)
+      stop(e)
     }
+  )
 
-    model
+  if (is.null(model)) {
+    stopf("Learner '%s' on task '%s' returned NULL during train_internal()", learner$id, task$id)
   }
 
+  model
+}
+
+
+# This wrapper calls learner$predict, and additionally performs some basic
+# checks that the prediction was successful.
+# Exceptions here are possibly encapsulated, so that they get captured and turned into log messages.
+predict_wrapper = function(task, learner) {
+  if (is.null(learner$data$model)) {
+    stopf("No trained model available for learner '%s' on task '%s'", learner$id, task$id)
+  }
+
+  result = tryCatch(
+    learner$predict_internal(task = task),
+    error = function(e) {
+      e$message = sprintf("Learner '%s' on task '%s' failed to predict: %s", learner$id, task$id, e$message)
+      stop(e)
+    }
+  )
+
+  if (!inherits(result, "Prediction")) {
+    stopf("Learner '%s' on task '%s' did not return a Prediction object, but instead: %s",
+      learner$id, task$id, as_short_string(result))
+  }
+
+  unsupported = setdiff(names(result$data), c("row_ids", "truth", learner$predict_types))
+  if (length(unsupported)) {
+    stopf("Learner '%s' on task '%s' returned result for unsupported predict type '%s'", learner$id, task$id, head(unsupported, 1L))
+  }
+
+  return(result)
+}
+
+
+learner_train = function(learner, task, row_ids = NULL, ctrl = mlr_control()) {
   task = assert_task(task)
 
   # subset to train set w/o cloning
@@ -29,9 +60,9 @@ learner_train = function(learner, task, row_ids = NULL, ctrl = mlr_control()) {
     task$row_roles$use = row_ids
   }
 
-  # call wrapper with encapsulation
+  # call train_wrapper with encapsulation
   result = encapsulate(learner$encapsulate["train"],
-    .f = wrapper,
+    .f = train_wrapper,
     .args = list(learner = learner, task = task),
     .pkgs = learner$packages,
     .seed = NA_integer_
@@ -61,34 +92,9 @@ learner_train = function(learner, task, row_ids = NULL, ctrl = mlr_control()) {
 
 
 learner_predict = function(learner, task, row_ids = NULL, ctrl = mlr_control()) {
-  wrapper = function(task, learner) {
-    if (is.null(learner$data$model)) {
-      stopf("No trained model available for learner '%s' on task '%s'", learner$id, task$id)
-    }
-
-    tryCatch(
-      { result = learner$predict_internal(task = task) },
-      error = function(e) {
-        e$message = sprintf("Learner '%s' on task '%s' failed to predict: %s", learner$id, task$id, e$message)
-        stop(e)
-      }
-    )
-
-    if (!inherits(result, "Prediction")) {
-      stopf("Learner '%s' on task '%s' did not return a Prediction object, but instead: %s",
-        learner$id, task$id, as_short_string(result))
-    }
-
-    unsupported = setdiff(names(result$data), c("row_ids", "truth", learner$predict_types))
-    if (length(unsupported)) {
-      stopf("Learner '%s' on task '%s' returned result for unsupported predict type '%s'", learner$id, task$id, head(unsupported, 1L))
-    }
-
-    return(result)
-  }
-
   task = assert_task(task)
 
+  # subset to test set w/o cloning
   if (!is.null(row_ids)) {
     row_ids = assert_row_ids(row_ids)
     prev_use = task$row_roles$use
@@ -96,12 +102,18 @@ learner_predict = function(learner, task, row_ids = NULL, ctrl = mlr_control()) 
     task$row_roles$use = row_ids
   }
 
-  prediction = NULL
-  if (!is.null(learner$model)) {
+  if (is.null(learner$model)) {
+    prediction = NULL
+    learner$data$predict_log = data.table(
+      class = factor("warning", levels = c("output", "warning", "error"), ordered = TRUE),
+      msg = "No model trained"
+    )
+    learner$data$predict_time = NA_real_
+  } else {
     # call predict with encapsulation
     result = encapsulate(
       learner$encapsulate["predict"],
-      .f = wrapper,
+      .f = predict_wrapper,
       .args = list(task = task, learner = learner),
       .pkgs = learner$packages,
       .seed = NA_integer_
@@ -112,12 +124,23 @@ learner_predict = function(learner, task, row_ids = NULL, ctrl = mlr_control()) 
     learner$data$predict_time = result$elapsed
   }
 
-  fb = learner$fallback
-  if (is.null(prediction) && !is.null(fb)) {
+  predict_fb = function(row_ids) {
     fb = assert_learner(fb)
+    fb$predict_type = learner$predict_type
     fb$data = learner$data$fallback_data
-    require_namespaces(fb$packages)
-    prediction = fb$predict(task)
+    prediction = fb$predict(task, row_ids)
+  }
+
+  fb = learner$fallback
+  if (!is.null(fb)) {
+    if (is.null(prediction)) {
+      prediction = predict_fb(task$row_ids)
+    } else {
+      miss_ids = prediction$missing
+      if (length(miss_ids)) {
+        prediction = c(prediction, predict_fb(miss_ids), keep_duplicates = FALSE)
+      }
+    }
   }
 
   return(prediction)
