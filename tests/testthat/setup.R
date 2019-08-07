c5e192e68b8e89b40736a7b092e88b49df886de9
Michel Lang <michellang@gmail.com>
2019-08-07 22:48:44 +0200

    fix for oldrel

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,6 +3,10 @@ old_opts = options(
   warnPartialMatchAttr = TRUE,
   warnPartialMatchDollar = TRUE
 )
+
+# https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
+old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
+
 old_threshold = lg$threshold
 old_plan = future::plan()
 lg$set_threshold("warn")
