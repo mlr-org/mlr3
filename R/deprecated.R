ec8f9dae513004cdad3c547f1e880a3c8c705d7e
Michel Lang <michellang@gmail.com>
2019-08-25 23:59:08 +0200

    fix deprecated message

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,7 +1,7 @@
 #' @export
 #' @rdname mlr_coercions
 as_task.character = function(x, clone = FALSE) {
-  .Deprecated("tsk", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("tsk", old = "Automatic object creation from strings in `mlr3`")
   assert_string(x)
   mlr_tasks$get(x)
 }
@@ -9,14 +9,14 @@ as_task.character = function(x, clone = FALSE) {
 #' @export
 #' @rdname mlr_coercions
 as_tasks.character = function(x, clone = FALSE) {
-  .Deprecated("tsk", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("tsk", old = "Automatic object creation from strings in `mlr3`")
   mlr_tasks$mget(x)
 }
 
 #' @rdname mlr_coercions
 #' @export
 as_learner.character = function(x, clone = FALSE)  {
-  .Deprecated("lrn", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("lrn", old = "Automatic object creation from strings in `mlr3`")
   assert_string(x)
   mlr_learners$get(x)
 }
@@ -24,14 +24,14 @@ as_learner.character = function(x, clone = FALSE)  {
 #' @export
 #' @rdname mlr_coercions
 as_learners.character = function(x, clone = FALSE)  {
-  .Deprecated("lrn", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("lrn", old = "Automatic object creation from strings in `mlr3`")
   mlr_learners$mget(x)
 }
 
 #' @export
 #' @rdname mlr_coercions
 as_resampling.character = function(x, clone = FALSE) {
-  .Deprecated("rsmp", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("rsmp", old = "Automatic object creation from strings in `mlr3`")
   assert_string(x)
   mlr_resamplings$get(x)
 }
@@ -39,14 +39,14 @@ as_resampling.character = function(x, clone = FALSE) {
 #' @export
 #' @rdname mlr_coercions
 as_resamplings.character = function(x, clone = FALSE) {
-  .Deprecated("rsmp", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("rsmp", old = "Automatic object creation from strings in `mlr3`")
   mlr_resamplings$mget(x)
 }
 
 #' @export
 #' @rdname mlr_coercions
 as_measure.character = function(x, task_type = NULL, clone = FALSE) {
-  .Deprecated("msr", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("msr", old = "Automatic object creation from strings in `mlr3`")
   assert_string(x)
   mlr_measures$get(x)
 }
@@ -54,6 +54,6 @@ as_measure.character = function(x, task_type = NULL, clone = FALSE) {
 #' @export
 #' @rdname mlr_coercions
 as_measures.character = function(x, task_type = NULL, clone = FALSE) {
-  .Deprecated("msr", package = "mlr3", old = "Automatic object creation from strings in `mlr3`")
+  .Deprecated("msr", old = "Automatic object creation from strings in `mlr3`")
   mlr_measures$mget(x)
 }
