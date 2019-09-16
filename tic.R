2ab14389ca79916259e3a6b08c88aa7542fb9c40
Patrick Schratz <patrick.schratz@gmail.com>
2019-09-16 10:06:45 +0200

    revert git submodule approach (#346)

--- tic.R
+++ tic.R
@@ -1,1 +1,8 @@
-mlr-ci/tic.R
+do_package_checks(error_on = "warning")
+
+if (ci_has_env("BUILD_PKGDOWN")) {
+    do_pkgdown(orphan = TRUE)
+}
+
+get_stage("after_success") %>%
+  add_code_step(system("curl -s https://raw.githubusercontent.com/mlr-org/mlr3orga/master/trigger-mlr3book.sh | bash"))
