5ade583f11c95cf913190f807098cb34219db5e4
Michel Lang <michellang@gmail.com>
2019-10-02 20:24:20 +0200

    Stratify as column role (fixes #364) (#371)

--- R/Task_mutators.R
+++ R/Task_mutators.R
@@ -18,33 +18,43 @@ task_set_row_role = function(self, rows, new_roles, exclusive = TRUE) {
 }
 
 task_set_col_role = function(self, cols, new_roles, exclusive = TRUE) {
-
   assert_character(cols, any.missing = FALSE)
   assert_subset(cols, self$col_info$id)
   assert_subset(new_roles, mlr_reflections$task_col_roles[[self$task_type]])
   assert_flag(exclusive)
 
+  col_roles = self$col_roles
+
   for (role in new_roles) {
-    self$col_roles[[role]] = union(self$col_roles[[role]], cols)
+    col_roles[[role]] = union(col_roles[[role]], cols)
   }
 
   if (exclusive) {
-    for (role in setdiff(names(self$col_roles), new_roles)) {
-      self$col_roles[[role]] = setdiff(self$col_roles[[role]], cols)
+    for (role in setdiff(names(col_roles), new_roles)) {
+      col_roles[[role]] = setdiff(col_roles[[role]], cols)
     }
   }
 
+  if (inherits(self, "TaskSupervised") && length(col_roles[["target"]]) == 0L) {
+    stopf("Supervised tasks need a target column")
+  }
+
+  for (role in c("groups", "weights")) {
+    if (length(col_roles[[role]]) > 1L)
+      stopf("Multiple columns with role '%s' not supported", role)
+  }
+
   # update weights and groups property
-  for (role in c("weights", "groups")) {
-    n = length(self$col_roles[[role]])
+  for (role in c("stratify", "groups", "weights")) {
+    n = length(col_roles[[role]])
     if (n == 0L) {
       self$properties = setdiff(self$properties, role)
     } else if (n == 1L) {
       self$properties = union(self$properties, role)
-    } else {
-      stopf("Multiple columns with role '%s' not supported", role)
     }
   }
+
+  self$col_roles = col_roles
 }
 
 check_new_row_ids = function(task, data, type) {
--- tests/testthat/test_mlr_resampling_repeated_cv.R
+++ tests/testthat/test_mlr_resampling_repeated_cv.R
@@ -19,8 +19,9 @@ test_that("folds first, then repetitions", {
 test_that("stratification", {
   data = data.table(y = rep(letters[1:2], times = c(90, 10)), x1 = runif(100), x2 = rep(LETTERS[1:2], times = c(50, 50)))
   task = TaskClassif$new("stratify_data", data, target = "y")
+  task$set_col_role(task$target_names, "stratify", FALSE)
 
-  r = rsmp("repeated_cv", folds = 5, repeats = 2, stratify = TRUE)
+  r = rsmp("repeated_cv", folds = 5, repeats = 2)
   r$instantiate(task)
 
   for (i in seq_len(r$iters)) {
