bed6490e644292ad967aa916791241fd65a0f66f
pat-s <patrick.schratz@gmail.com>
2019-10-21 11:50:04 +0200

    install mlr3pkgdowntemplate in install stage so it gets cached

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
 do_package_checks(error_on = "warning")
 
 if (ci_has_env("BUILD_PKGDOWN")) {
-  get_stage("before_deploy") %>%
+  get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown(orphan = TRUE)
 }
