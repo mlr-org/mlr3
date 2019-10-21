748ed89ee141154a79085680ea6d628bc0645fba
Michel Lang <michellang@gmail.com>
2019-10-21 21:34:26 +0200

    add predict function (fixes #212)

---
+++ R/predict.R
@@ -0,0 +1,66 @@
+#' @title Predict Method for Learners
+#'
+#' @description
+#' Extends the generic [stats::predict()] with a method for [Learner].
+#' Note that this function is intended as glue code to be used in third party packages.
+#' We recommend to work with the [Learner] directly, i.e. calling `learner$predict()`
+#' or `learner$predict_newdata()` directly.
+#'
+#' Performs the following steps:
+#' * Sets additional hyperparameters passed to this function.
+#' * Creates a [Prediction] object by calling `learner$predict_newdata()`.
+#' * Returns (subset of) [Prediction].
+#'
+#'
+#'
+#' @param object :: [Learner]\cr
+#'   Any [Learner].
+#' @param newdata :: [data.frame()]\cr
+#'   New data to predict on.
+#' @param predict_type :: `character(1)`\cr
+#'   The predict type to return.
+#'   Set to `<Prediction>` to retrieve the complete [Prediction] object.
+#'   If set to `NULL` (default), the first predict type for the respective class of the [Learner]
+#'   as stored in [mlr_reflections] is used.
+#' @param ... :: `any`\cr
+#'   Hyperparameters to pass down to the [Learner].
+#'
+#' @export
+#' @examples
+#' task = tsk("spam")
+#'
+#' learner = lrn("classif.rpart", predict_type = "prob")
+#' learner$train(task)
+#' predict(learner, task$data(1:3), predict_type = "response")
+#' predict(learner, task$data(1:3), predict_type = "prob")
+#' predict(learner, task$data(1:3), predict_type = "<Prediction>")
+predict.Learner = function(object, newdata, predict_type = NULL, ...) {
+  assert_data_frame(newdata)
+  assert_string(predict_type, null.ok = TRUE)
+
+  if (...length()) {
+    pars = assert_list(list(...), names = "unique")
+    tags = object$param_set$tags
+    predict_pars = names(tags)[map_lgl(tags, is.element, el = "predict")]
+    i = which(names(pars) %nin% predict_pars)
+    if (length(i)) {
+      stopf("Unknown parameters: %s", str_collapse(names(pars)[i]))
+    }
+
+    object = object$clone()
+    object$param_set$values = insert_named(object$param_set$values, list(...))
+  }
+
+  prediction = object$predict_newdata(newdata)
+
+  if (identical(predict_type, "<Prediction>")) {
+    return(prediction)
+  }
+
+  predict_type = predict_type %??% head(names(mlr_reflections$learner_predict_types[[object$task_type]]), 1L)
+  if (predict_type %nin% prediction$predict_types) {
+    stopf("Predict type '%s' not available", predict_type)
+  }
+
+  prediction[[predict_type]]
+}
--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -1,144 +1,18 @@
 context("predict")
 
+test_that("predict method works", {
+  task = tsk("sonar")
+  lrn = lrn("classif.featureless")$train(task)
 
-test_that("predict on newdata works / classif", {
-  task = tsk("iris")$filter(1:120)
-  learner = lrn("classif.featureless")
-  expect_error(learner$predict(task), "trained")
-  learner$train(task)
-  expect_task(learner$state$train_task)
-  newdata = tsk("iris")$filter(121:150)$data()
+  newdata = task$data(1:3)
+  expect_factor(predict(lrn, newdata = newdata), len = 3)
+  expect_factor(predict(lrn, newdata = newdata, predict_type = "response"), len = 3)
+  expect_error(predict(lrn, newdata = newdata, predict_type = "prob"), "'prob'")
 
-  # passing the task
-  p = learner$predict_newdata(newdata = newdata, task = task)
-  expect_data_table(as.data.table(p), nrows = 30)
-  expect_set_equal(as.data.table(p)$row_id, 1:30)
-  expect_factor(p$truth, any.missing = FALSE, levels = task$class_names)
+  lrn = lrn("classif.featureless", predict_type = "prob")$train(task)
+  expect_factor(predict(lrn, newdata = newdata), len = 3)
+  expect_factor(predict(lrn, newdata = newdata, predict_type = "response"), len = 3)
+  expect_matrix(predict(lrn, newdata = newdata, predict_type = "prob"), nrows = 3, ncols = 2)
 
-  # rely on internally stored task representation
-  p = learner$predict_newdata(newdata = newdata, task = NULL)
-  expect_data_table(as.data.table(p), nrows = 30)
-  expect_set_equal(as.data.table(p)$row_id, 1:30)
-  expect_factor(p$truth, any.missing = FALSE, levels = task$class_names)
-
-  # with missing target column
-  newdata$Species = NULL
-  p = learner$predict_newdata(newdata = newdata, task = task)
-  expect_data_table(as.data.table(p), nrows = 30)
-  expect_set_equal(as.data.table(p)$row_id, 1:30)
-  expect_factor(p$truth, levels = task$class_names)
-  expect_true(allMissing(p$truth))
-})
-
-
-test_that("train task is properly cloned (#383)", {
-  lr = lrn("classif.rpart")$train(tsk("iris"))
-  p = lr$predict_newdata(iris[1:3, 1:4])
-  expect_equal(length(p$row_ids), 3)
-  p = lr$predict_newdata(iris[1:3, 1:4])
-  expect_equal(length(p$row_ids), 3)
-})
-
-test_that("predict on newdata works / regr", {
-  task = tsk("boston_housing")
-  train = which(seq_len(task$nrow) %% 2 == 0L)
-  test = setdiff(seq_len(task$nrow), train)
-
-  learner = lrn("regr.featureless")
-  learner$train(task)
-
-  newdata = task$clone()$filter(test)$data()
-  p = learner$predict_newdata(newdata)
-
-  expect_data_table(as.data.table(p), nrows = length(test))
-  expect_set_equal(as.data.table(p)$row_id, seq_along(test))
-})
-
-
-test_that("predict on newdata works / no target column", {
-  task = tsk("boston_housing")
-  train = which(seq_len(task$nrow) %% 2 == 0L)
-  test = setdiff(seq_len(task$nrow), train)
-
-  learner = lrn("regr.featureless")
-  learner$train(task$clone()$filter(train))
-
-  newdata = remove_named(task$clone()$filter(test)$data(), task$target_names)
-  p = learner$predict_newdata(newdata = newdata)
-
-  expect_data_table(as.data.table(p), nrows = length(test))
-  expect_set_equal(as.data.table(p)$row_id, seq_along(test))
-})
-
-
-test_that("predict on newdata works / titanic use case", {
-  skip_if_not_installed("titanic")
-  train = load_dataset("titanic_train", package = "titanic")
-  test = load_dataset("titanic_test", package = "titanic")
-  drop = c("Cabin", "Name", "Ticket", "PassengerId")
-
-  train = remove_named(train, drop)
-  test = remove_named(test, drop)
-
-  train$Embarked = factor(train$Embarked)
-  test$Embarked = factor(test$Embarked, levels = levels(train$Embarked))
-  train$Sex = factor(train$Sex)
-  test$Sex = factor(test$Sex, levels = levels(train$Sex))
-
-  median_age = median(train$Age, na.rm = TRUE)
-  train$Age[is.na(train$Age)] = median_age
-  test$Age[is.na(test$Age)] = median_age
-
-  train$Survived = factor(train$Survived)
-
-  train$Embarked[train$Embarked == ""] = NA
-  train$Embarked = droplevels(train$Embarked)
-  test$Embarked[test$Embarked == ""] = NA
-  test$Embarked = droplevels(test$Embarked)
-
-  task = TaskClassif$new(id = "titanic", train, target = "Survived", positive = "1")
-
-  lrn = lrn("classif.rpart")
-
-  lrn$train(task)
-  p = lrn$predict_newdata(newdata = test)
-  expect_prediction_classif(p)
-  expect_factor(p$response, levels = task$class_names, any.missing = FALSE)
-  expect_factor(p$truth, levels = task$class_names)
-  expect_true(allMissing(p$truth))
-})
-
-test_that("predict train + test set", {
-  task = tsk("iris")
-  m1 = msr("debug", id = "tr", predict_sets = "train")
-  m2 = msr("debug", id = "te", predict_sets = "test")
-  m3 = msr("debug", id = "trte", predict_sets = c("train", "test"))
-  measures = list(m1, m2, m3)
-  hout = rsmp("holdout")$instantiate(task)
-  n_train = length(hout$train_set(1))
-  n_test = length(hout$test_set(1))
-
-  learner = lrn("classif.rpart")
-  rr = resample(task, learner, hout)
-  expect_error(rr$aggregate(measures = measures), "predict.set")
-
-  learner = lrn("classif.rpart", predict_sets = "train")
-  rr = resample(task, learner, hout)
-  expect_error(rr$aggregate(measures = measures), "predict.set")
-
-  learner = lrn("classif.rpart", predict_sets = c("train", "test"))
-  rr = resample(task, learner, hout)
-  aggr = rr$aggregate(measures = measures)
-  expect_equal(unname(is.na(aggr)), c(FALSE, FALSE, FALSE))
-  expect_equal(unname(aggr), c(n_train, n_test, n_train + n_test))
-})
-
-test_that("assertions work (#357)", {
-  measures = lapply(c("classif.auc","classif.acc"), msr)
-  task = tsk("iris")
-  lrn = lrn("classif.featureless")
-  p = lrn$train(task)$predict(task)
-
-  expect_error(p$score(msr("classif.auc"), "predict.type"))
-  expect_error(p$score(msr("regr.mse"), "task.type"))
+  expect_true(uniqueN(predict(lrn, newdata, method = "mode")) == 1L)
 })
