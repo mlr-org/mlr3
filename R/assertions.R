61f36f9d1e348e7d5b84ecf9ac821bc2bc211443
Michel Lang <michellang@gmail.com>
2020-01-13 12:12:01 +0100

    improve error message in assertion

--- R/assertions.R
+++ R/assertions.R
@@ -141,7 +141,8 @@ assert_measure = function(measure, task = NULL, learner = NULL, .var.name = vnam
 
     miss = setdiff(measure$predict_sets, learner$predict_sets)
     if (length(miss)) {
-      stopf("Measure '%s' needs predict set '%s'", measure$id, str_collapse(miss))
+      stopf("Measure '%s' needs predict set '%s', but learner '%s' only predicted on sets '%s'",
+        measure$id, str_collapse(miss), learner$id, str_collapse(learner$predict_sets))
     }
   }
 
