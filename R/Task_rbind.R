913d0fff91c562795cffc2d4c1ec4e038c42bb29
Michel Lang <michellang@gmail.com>
2020-03-05 01:02:35 +0100

    cleanup mutators

--- R/Task_rbind.R
+++ R/Task_rbind.R
@@ -1,34 +1,34 @@
 task_rbind = function(backend, task) {
-  UseMethod("task_rbind")
-}
-
-task_rbind.data.frame = function(backend, task) {
-  if (any(dim(backend) == 0L)) {
-    return(invisible(task))
-  }
-
   pk = task$backend$primary_key
   rn = task$backend$rownames
+  pk_in_backend = TRUE
 
-  if (pk %nin% names(backend)) {
-    start = if (length(rn)) max(rn) + 1L else 1L
-    pk = seq(from = start, to = start + nrow(backend) - 1L)
+  if (is.data.frame(backend)) {
+    pk_in_backend = pk %in% names(backend)
+
+    if (ncol(backend) == pk_in_backend || nrow(backend) == 0L) {
+      return(invisible(task))
+    }
+
+    if (!pk_in_backend) {
+      start = if (length(rn)) max(rn) + 1L else 1L
+      pk = seq(from = start, to = start + nrow(backend) - 1L)
+    }
+
+    ci = task$col_info[list(names(backend)), on = "id", nomatch = NULL]
+    backend = do.call(data.table, Map(auto_convert,
+      value = as.list(backend)[ci$id],
+      id = ci$id, type = ci$type, levels = ci$levels))
+
+    backend = as_data_backend(backend, primary_key = pk)
+  } else {
+    assert_backend(backend)
+    if (backend$ncol <= 1L || backend$nrow == 0L) {
+      return(invisible(task))
+    }
   }
 
-  ci = task$col_info[list(names(backend)), on = "id", nomatch = NULL]
-  backend = do.call(data.table, Map(auto_convert,
-    value = as.list(backend)[ci$id],
-    id = ci$id, type = ci$type, levels = ci$levels))
-
-  task_rbind(as_data_backend(backend, primary_key = pk), task)
-}
-
-task_rbind.DataBackend = function(backend, task) {
-  if (backend$ncol <= 1L) {
-    return(invisible(task))
-  }
-
-  if (any(backend$rownames %in% task$backend$rownames)) {
+  if (pk_in_backend && any(backend$rownames %in% task$backend$rownames)) {
     stopf("Cannot rbind data to task '%s', duplicated row ids", task$id)
   }
 
@@ -40,19 +40,23 @@ task_rbind.DataBackend = function(backend, task) {
     stopf("Cannot rbind data to task '%s', missing the following mandatory columns: %s", task$id, str_collapse(missing_cols))
   }
 
+  # merge col infos
   tab = merge(task$col_info, col_info(backend), by = "id",
     all.x = TRUE, all.y = FALSE, suffixes = c("", ".y"))
   levels = levels.y = type = type.y = NULL
 
+  # type check
   ii = head(tab[type != type.y, which = TRUE], 1L)
   if (length(ii)) {
     stopf("Cannot rbind to task: Types do not match for column: %s (%s != %s)", tab$id[ii], tab$type[ii], tab$type.y[ii])
   }
 
+  # merge factor levels
   vunion = function(x, y) Map(union, x, y)
   tab[type %in% c("factor", "ordered"), levels := list(vunion(levels, levels.y))]
   tab[, c("type.y", "levels.y") := list(NULL, NULL)]
 
+  # everything looks good, modify task
   task$backend = DataBackendRbind$new(task$backend, backend)
   task$col_info = tab
   task$row_roles$use = c(task$row_roles$use, backend$rownames)
