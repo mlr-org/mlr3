438c6668e46724bbd7f3cebcac37840f077282bf
pat-s <patrick.schratz@gmail.com>
2020-03-15 23:12:09 +0100

    assert properties in assert_learnable()

--- R/assertions.R
+++ R/assertions.R
@@ -105,6 +105,11 @@ assert_learnable = function(task, learner) {
   if (length(tmp)) {
     stopf("%s has the following unsupported feature types: %s", task$format(), str_collapse(tmp))
   }
+
+  tmp = setdiff(task$properties, learner$properties)
+  if (length(tmp)) {
+    stopf("%s has the following unsupported properties: %s", task$format(), str_collapse(tmp))
+  }
 }
 
 #' @export
