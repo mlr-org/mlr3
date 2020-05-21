a99e3dc5ec700a67217110ff71b071861f2bba60
pat-s <patrick.schratz@gmail.com>
2020-05-21 09:30:13 +0200

    add mlr3pkgdowntemplate

--- tic.R
+++ tic.R
@@ -4,5 +4,7 @@ do_package_checks()
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
+  get_stage("install") %>%
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
 }
