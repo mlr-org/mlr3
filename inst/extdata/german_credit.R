eb483dc941fbbcadce06b2591881798e2a6c3498
Lennart Schneider <lennart.sch@web.de>
2020-07-20 12:41:22 +0200

    replace german_credit data with corrected data from rchallenge package (#514)

--- inst/extdata/german_credit.R
+++ inst/extdata/german_credit.R
@@ -1,3 +1,3 @@
 root = rprojroot::find_package_root_file()
-data = mlr3misc::load_dataset("GermanCredit", "evtree")
+data = mlr3misc::load_dataset("german", "rchallenge")
 saveRDS(data, file = file.path(root, "inst", "extdata", "german_credit.rds"), version = 2L)
