8fccf10b710a03a00833010c6d5f870073e68269
Michel Lang <michellang@gmail.com>
2020-08-05 14:37:50 +0200

    add breast cancer task

---
+++ R/TaskClassif_breast_cancer.R
@@ -0,0 +1,39 @@
+#' @title Wisconsin Breast Cancer Classification Task
+#'
+#' @name mlr_tasks_breast_cancer
+#' @format [R6::R6Class] inheriting from [TaskClassif].
+#' @include mlr_tasks.R
+#'
+#' @description
+#' A classification task for the [mlbench::BreastCancer] data set.
+#'
+#' * Column `"Id"` has been removed.
+#' * Column names have been converted to `snake_case`.
+#' * Positive class is set to `"malignant"`.
+#' * 16 incomplete cases have been removed from the data set.
+#'
+#' @section Construction:
+#' ```
+#' mlr_tasks$get("breast_cancer")
+#' tsk("breast_cancer")
+#' ```
+#'
+#' @section Meta Information:
+#' `r rd_info(tsk("breast_cancer"))`
+#'
+#'
+#' @template seealso_task
+NULL
+
+load_task_breast_cancer = function(id = "breast_cancer") {
+  tab = setDT(load_dataset("BreastCancer", "mlbench"))
+  setnames(tab, new = tolower(chartr(".", "_", colnames(tab))))
+  b = as_data_backend(remove_named(tab[complete.cases(tab)], "Id"))
+
+  task = TaskClassif$new(id, b, target = "class", positive = "malignant")
+  b$hash = task$man = "mlr3::mlr_tasks_breast_cancer"
+  task
+}
+
+#' @include mlr_tasks.R
+mlr_tasks$add("breast_cancer", load_task_breast_cancer)
