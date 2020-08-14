d41ed7323bc55827bd3c457813bb93fbd24ad2b6
Michel Lang <michellang@gmail.com>
2020-08-14 21:12:42 +0200

    cleanup generators

--- R/TaskGeneratorCassini.R
+++ R/TaskGeneratorCassini.R
@@ -40,7 +40,7 @@ TaskGeneratorCassini = R6Class("TaskGeneratorCassini",
     #'   Point char. Passed to [plot()].
     #' @param ... (any)\cr
     #'   Additional arguments passed to [plot()].
-    plot = function(n = 200, pch = 19L, ...) {
+    plot = function(n = 200L, pch = 19L, ...) {
       plot(private$.generate_obj(n), pch = pch, ...)
     }
   ),
