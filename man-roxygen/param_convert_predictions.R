c44232cac714b64b29fa8a6f04c92e912eadd126
Michel Lang <michellang@gmail.com>
2020-09-09 14:29:27 +0200

    update docs

---
+++ man-roxygen/param_convert_predictions.R
@@ -0,0 +1,4 @@
+#' @param convert_predictions (`logical(1)`)\cr
+#'   If `TRUE` (default), the converts the internal [PredictionData] objects to
+#'   regular [Prediction] objects.
+#'   Uses all predict sets specified via `predict_sets`.
