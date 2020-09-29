b963730c79de5da51c490747a392a142808aa727
Marc Becker <marcbecker@posteo.de>
2020-09-29 12:16:51 +0200

    Add resample and benchmark continue unit tests

--- tests/testthat/test_continue.R
+++ tests/testthat/test_continue.R
@@ -38,3 +38,57 @@ test_that("continue method works", {
   expect_error(learner$continue(task),
     regexp = "Supplied task does not allow to continue training")
 })
+
+test_that("resample_continue works", {
+  task = tsk("iris")
+  learner = lrn("classif.xgboost", nrounds = 10)
+  resampling = rsmp("cv", folds = 3)
+  rr = resample(task, learner, resampling, store_models = TRUE)
+
+  learner$param_set$values$nrounds = 20
+  rr2 = resample_continue(learner, rr, store_models = TRUE)
+
+  expect_equal(length(rr$learners), 3)
+  map(rr2$learners, function(l) {
+    expect_equal(l$param_set$values$nrounds, 20)
+    expect_class(l$model, "xgb.Booster")
+    expect_equal(l$model$niter, 20)
+  })
+  expect_false(identical(rr2$learners[[1]]$model$raw, rr2$learners[[2]]$model$raw))
+  expect_false(identical(rr2$learners[[1]]$model$raw, rr2$learners[[3]]$model$raw))
+  expect_false(identical(rr2$learners[[2]]$model$raw, rr2$learners[[3]]$model$raw))
+})
+
+test_that("benchmark_continue works", {
+  task = tsk("iris")
+  learner1 = lrn("classif.xgboost", nrounds = 10, eta = 0.1)
+  learner1$id = "classif.xgboost_1"
+  learner2 = lrn("classif.xgboost", nrounds = 10, eta = 0.5)
+  learner2$id = "classif.xgboost_2"
+  learners = list(learner1, learner2)
+  resampling = rsmp("cv", folds = 3)
+
+
+  design = benchmark_grid(task, learners, resampling)
+  bmr = benchmark(design, store_models = TRUE)
+
+  learners[[1]]$param_set$values$nrounds = 20
+  learners[[2]]$param_set$values$nrounds = 20
+
+  bmr2 = benchmark_continue(learners, bmr, store_models = TRUE)
+
+  eta = c(0.1, 0.5)
+  map(seq(bmr2$n_resample_results), function(i) {
+    learners = bmr2$resample_result(i)$learners
+    expect_equal(length(learners), 3)
+    map(learners, function(l) {
+      expect_equal(l$param_set$values$nrounds, 20)
+      expect_class(l$model, "xgb.Booster")
+      expect_equal(l$model$niter, 20)
+      expect_equal(l$param_set$values$eta, eta[i])
+    })
+  })
+
+  expect_false(identical(bmr2$resample_result(1)$learners[[1]]$model$raw,
+                         bmr2$resample_result(2)$learners[[1]]$model$raw))
+})
