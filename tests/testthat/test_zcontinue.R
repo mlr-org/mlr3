b0d069711e85d9e075051d4143a0f243db8f6c7e
Marc Becker <marcbecker@posteo.de>
2020-09-30 11:42:04 +0200

    Test unit tests

--- tests/testthat/test_zcontinue.R
+++ tests/testthat/test_zcontinue.R
@@ -1,27 +1,23 @@
 test_that("continue method works", {
     task = tsk("iris")
     learner = mlr3learners::LearnerClassifXgboost$new()
-    learner$param_set$values$nrounds = 10
+    learner$param_set$values$nrounds = 5
 
     learner$train(task)
     m1 = learner$model
 
-    learner$param_set$values$nrounds = 20
+    learner$param_set$values$nrounds = 10
     learner$continue(task)
     m2 = learner$model
 
     expect_equal(learner$param_set$values$nrounds, learner$model$niter)
     expect_false(identical(m1, m2))
 
-    learner = mlr3learners::LearnerClassifXgboost$new()
-    learner$param_set$values$nrounds = 10
-    learner$train(task)
-
     expect_error(learner$continue(task),
                  regexp = "No additional boosting iterations provided")
 
     learner = mlr3learners::LearnerClassifXgboost$new()
-    learner$param_set$values$nrounds = 10
+    learner$param_set$values$nrounds = 5
 
     expect_error(learner$continue(task),
                  regexp = "Learner does not contain a model")
@@ -34,7 +30,7 @@ test_that("continue method works", {
 
     task = tsk("iris")
     learner = mlr3learners::LearnerClassifXgboost$new()
-    learner$param_set$values$nrounds = 10
+    learner$param_set$values$nrounds = 5
     learner$train(task)
     task$select("Petal.Length")
 
@@ -46,18 +42,18 @@ test_that("resample_continue works", {
 
   task = tsk("iris")
   learner = mlr3learners::LearnerClassifXgboost$new()
-  learner$param_set$values$nrounds = 10
+  learner$param_set$values$nrounds = 5
   resampling = rsmp("cv", folds = 3)
   rr = resample(task, learner, resampling, store_models = TRUE)
 
-  learner$param_set$values$nrounds = 20
+  learner$param_set$values$nrounds = 10
   rr2 = resample_continue(learner, rr, store_models = TRUE)
 
   expect_equal(length(rr$learners), 3)
   map(rr2$learners, function(l) {
-    expect_equal(l$param_set$values$nrounds, 20)
+    expect_equal(l$param_set$values$nrounds, 10)
     expect_class(l$model, "xgb.Booster")
-    expect_equal(l$model$niter, 20)
+    expect_equal(l$model$niter, 10)
   })
   expect_false(identical(rr2$learners[[1]]$model$raw, rr2$learners[[2]]$model$raw))
   expect_false(identical(rr2$learners[[1]]$model$raw, rr2$learners[[3]]$model$raw))
@@ -68,13 +64,13 @@ test_that("resample_continue works", {
 test_that("benchmark_continue works", {
   task = tsk("iris")
   learner1 = mlr3learners::LearnerClassifXgboost$new()
-  learner1$param_set$values$nrounds = 10
-  learner1$param_set$values$eta = 0.1
+  learner1$param_set$values$nrounds = 5
+  learner1$param_set$values$eta = 0.3
   learner1$id = "classif.xgboost_1"
 
   learner2 = mlr3learners::LearnerClassifXgboost$new()
-  learner2$param_set$values$nrounds = 10
-  learner2$param_set$values$eta = 0.5
+  learner2$param_set$values$nrounds = 5
+  learner2$param_set$values$eta = 0.4
   learner2$id = "classif.xgboost_1"
   learners = list(learner1, learner2)
   resampling = rsmp("cv", folds = 3)
@@ -83,19 +79,19 @@ test_that("benchmark_continue works", {
   design = benchmark_grid(task, learners, resampling)
   bmr = benchmark(design, store_models = TRUE)
 
-  learners[[1]]$param_set$values$nrounds = 20
-  learners[[2]]$param_set$values$nrounds = 20
+  learners[[1]]$param_set$values$nrounds = 10
+  learners[[2]]$param_set$values$nrounds = 10
 
   bmr2 = benchmark_continue(learners, bmr, store_models = TRUE)
 
-  eta = c(0.1, 0.5)
+  eta = c(0.3, 0.4)
   map(seq(bmr2$n_resample_results), function(i) {
     learners = bmr2$resample_result(i)$learners
     expect_equal(length(learners), 3)
     map(learners, function(l) {
-      expect_equal(l$param_set$values$nrounds, 20)
+      expect_equal(l$param_set$values$nrounds, 10)
       expect_class(l$model, "xgb.Booster")
-      expect_equal(l$model$niter, 20)
+      expect_equal(l$model$niter, 10)
       expect_equal(l$param_set$values$eta, eta[i])
     })
   })
