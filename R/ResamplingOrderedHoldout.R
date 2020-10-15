33c3e76a1f7b703b8cef7a4696d41ef71e6d04e7
Michel Lang <michellang@gmail.com>
2020-10-15 15:46:34 +0200

    ...

--- R/ResamplingOrderedHoldout.R
+++ R/ResamplingOrderedHoldout.R
@@ -5,7 +5,6 @@
 #'
 #' @description
 #' Splits data into a training set and a test set, according to columns with column role `order`.
-#' Parameter `ratio` determines the ratio of observation going into the training set (default: 2/3).
 #'
 #' @templateVar id ordered_holdout
 #' @template section_dictionary_resampling
@@ -13,18 +12,38 @@
 #' @section Parameters:
 #' * `ratio` (`numeric(1)`)\cr
 #'   Ratio of observations to put into the training set.
+#'   Mutually exclusive with parameter `n`.
+#' * `n` (`integer(1)`)\cr
+#'   Number of observations to put into the training set.
+#'   If negative, the absolute value determines the number of observations in the test set.
+#'   Mutually exclusive with parameter `ratio`.
+#' * `unit` (`character(1)`)\cr
+#'   Either `"rows"` (default) to split based on the row number, or, if there is a single order column
+#'   of type [POSIXct], one of `"secs"`, `"mins"`, `"hours"`, `"days"`, `"months"`, or `"years"`.
+#'   For the latter, the ratio (`ratio`) or number of observations (`n`) correspond to the respective
+#'   time unit.
 #'
 #' @template seealso_resampling
 #' @export
+#' @examples
+#' # task airpassengers is ordered by monthly column 'date'
+#' task = tsk("airpassengers")
+#'
+#' # predict on last 12 months
+#' r = rsmp("ordered_holdout", n = -12)$instantiate(task)
+#' r$train_set(1)
+#' r$test_set(1)
 ResamplingOrderedHoldout = R6Class("ResamplingOrderedHoldout", inherit = Resampling,
   public = list(
     #' @description
     #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
       ps = ParamSet$new(list(
-        ParamDbl$new("ratio", lower = 0, upper = 1, tags = "required")
+        ParamDbl$new("ratio", lower = 0, upper = 1, default = 2 / 3),
+        ParamInt$new("n"),
+        ParamFct$new("unit", levels = c("rows", "secs", "mins", "hours", "days", "months", "years"), default = "rows", tags = "required")
       ))
-      ps$values = list(ratio = 2 / 3)
+      ps$values = list(unit = "rows")
 
       super$initialize(id = "ordered_holdout", param_set = ps, man = "mlr3::mlr_resamplings_ordered_holdout")
     },
@@ -38,9 +57,43 @@ ResamplingOrderedHoldout = R6Class("ResamplingOrderedHoldout", inherit = Resampl
       if ("ordered" %nin% task$properties) {
         stopf("%s requires an ordered task, but Task '%s' has no order", self$id, task$id)
       }
-      nr = round(length(ids) * self$param_set$values$ratio)
-      ids = ids[task$order(ids)]
-      list(train = head(ids, nr), test = tail(ids, -nr))
+      pv = self$param_set$values
+
+      get_training_size = function(n_obs) {
+        if (!xor(is.null(pv$ratio), is.null(pv$n))) {
+          stopf("Either parameter `ratio` (x)or `n` must be provided")
+        }
+
+        if (!is.null(pv$ratio)) {
+          round(n_obs * pv$ratio)
+        } else {
+          if (pv$n > 0L) {
+            min(n_obs, pv$n)
+          } else {
+            max(n_obs + pv$n, 0L)
+          }
+        }
+      }
+
+      if (pv$unit == "rows") {
+        ids = ids[task$order(ids)]
+        n = get_training_size(length(ids))
+        list(train = head(ids, n), test = tail(ids, -n))
+      } else {
+        order_cols = task$col_roles$order
+        if (length(order_cols) != 1L || fget(task$col_info, order_cols, "type", "id") != "POSIXct") {
+          stopf("For 'unit' != \"rows\", the task must be ordered by a single POSIXct column")
+        }
+
+        t = row_id = NULL
+        tab = setnames(task$backend$data(rows = ids, cols = c(task$backend$primary_key, order_cols)), c("row_id", "t"))
+        tab = tab[
+          order(t),
+          list(row_id = list(row_id)),
+          by = as.POSIXct(trunc(t, units = pv$unit))]
+        n = get_training_size(nrow(tab))
+        list(train = unlist(head(tab$row_id, n), use.names = FALSE), test = unlist(tail(tab$row_id, -n), use.names = FALSE))
+      }
     },
 
     .get_train = function(i) {
--- tests/testthat/test_mlr_resampling_ordered_holdout.R
+++ tests/testthat/test_mlr_resampling_ordered_holdout.R
@@ -16,3 +16,52 @@ test_that("order", {
 
   expect_lt(max(task$data(r$train_set(1))$date), min(task$data(r$test_set(1))$date))
 })
+
+test_that("order with posix", {
+  data = data.table(
+    y = sample(seq(from = Sys.time() - 24 * 3600, to = Sys.time(), length.out = 20)),
+    x = runif(20),
+    class = factor(sample(c("a", "b"), 20, replace = TRUE), levels = c("a", "b"))
+  )
+  task = TaskClassif$new("test", data, target = "class")
+  task$col_roles$order = "y"
+  expect_posixct(task$data()$y, sorted = TRUE)
+
+  r = rsmp("ordered_holdout")$instantiate(task)
+  expect_lt(max(task$data(r$train_set(1))$y), min(task$data(r$test_set(1))$y))
+})
+
+test_that("order with posix and time unit", {
+  data1 = data.table(
+    y = seq(from = Sys.time() - 24 * 3600, to = Sys.time(), length.out = 24),
+    x = runif(24),
+    class = factor(sample(c("a", "b"), 24, replace = TRUE), levels = c("a", "b"))
+  )
+  data2 = data.table(
+    y = seq(from = Sys.time() - 12 * 3600, to = Sys.time(), length.out = 12),
+    x = runif(12),
+    class = factor(sample(c("a", "b"), 12, replace = TRUE), levels = c("a", "b"))
+  )
+  data = rbind(data1, data2)
+
+  task = TaskClassif$new("test", data, target = "class")
+  task$col_roles$order = "y"
+  expect_posixct(task$data()$y, sorted = TRUE)
+
+  r = rsmp("ordered_holdout", unit = "hours", ratio = 0.5)$instantiate(task)
+  length(r$train_set(1))
+  length(r$test_set(1))
+
+  expect_lt(max(task$data(r$train_set(1))$y), min(task$data(r$test_set(1))$y))
+
+
+
+
+  r = rsmp("ordered_holdout", unit = "hours", n = 5)$instantiate(task)
+  length(r$train_set(1))
+  length(r$test_set(1))
+
+  r = rsmp("ordered_holdout", unit = "hours", n = -5)$instantiate(task)
+  length(r$train_set(1))
+  length(r$test_set(1))
+})
