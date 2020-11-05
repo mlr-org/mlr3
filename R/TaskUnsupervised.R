0c140760be99d29bd5bc49732eab651c96b72295
damirpolat <dpulatov@uwyo.edu>
2020-11-04 17:33:14 -0700

    remove target from col roles in unsupervised task

--- R/TaskUnsupervised.R
+++ R/TaskUnsupervised.R
@@ -14,5 +14,16 @@
 #' @keywords internal
 #' @export
 #' @examples
-#' TaskUnsupervised$new("iris", task_type = "regr", backend = iris)
-TaskUnsupervised = R6Class("TaskUnsupervised", inherit = Task)
+#' library(mlr3cluster)
+#' TaskUnsupervised$new("iris", task_type = "clust", backend = iris[, -5])
+TaskUnsupervised = R6Class("TaskUnsupervised", inherit = Task,
+  public = list(
+   #' @description
+   #' Creates a new instance of this [R6][R6::R6Class] class.
+   initialize = function(id, task_type, backend, extra_args = list()) {
+     super$initialize(id = id, task_type = task_type, backend = backend, extra_args = extra_args)
+     self$col_roles$target = NULL
+     self$col_roles$feature = self$col_roles$feature
+   }
+  )
+)
