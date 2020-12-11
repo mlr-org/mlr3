c6f7ead7fc24c6ca45b8a3ae928c840f69f19c29
Marc Becker <marcbecker@posteo.de>
2020-12-11 14:18:24 +0100

    Fix space

--- R/zzz.R
+++ R/zzz.R
@@ -55,7 +55,7 @@ dummy_import = function() {
   lg = lgr::get_logger(pkgname)
   assign("lg", lg, envir = parent.env(environment()))
   f = function(event) {
-    event$msg = paste("[mlr3]", event$msg)
+    event$msg = paste0("[mlr3]  ", event$msg)
     TRUE
   }
   lg$add_filter(f)
