08fd01ab55d37051e06a9ca2cb22ff6da9fec2f9
Michel Lang <michellang@gmail.com>
2021-02-03 22:08:42 +0100

    reorganize files

---
+++ R/as_prediction_data.R
@@ -0,0 +1,47 @@
+#' @title PredictionData
+#'
+#' @description
+#' Convert object to a [PredictionData] or a list of [PredictionData].
+#'
+#' @inheritParams as_task
+#'
+#' @param task ([Task]).
+#' @param row_ids (`integer()`).
+#' @param check (`logical(1)`)\cr
+#'   Perform argument checks and type conversions?
+#' @return [PredictionData].
+#' @export
+as_prediction_data = function(x, task, row_ids = task$row_ids, check = TRUE, ...) {
+  UseMethod("as_prediction_data")
+}
+
+#' @rdname as_prediction_data
+#' @export
+as_prediction_data.Prediction = function(x, task, row_ids = task$row_ids, check = TRUE, ...) { # nolint
+  x$data
+}
+
+#' @rdname as_prediction_data
+#' @export
+as_prediction_data.PredictionData = function(x, task, row_ids = task$row_ids, check = TRUE, ...) { # nolint
+  x
+}
+
+#' @rdname as_prediction_data
+#' @export
+as_prediction_data.list = function(x, task, row_ids = task$row_ids, check = TRUE, ...) { # nolint
+  assert_list(x, names = "unique")
+  predict_types = names(mlr_reflections$learner_predict_types[[task$task_type]])
+  assert_names(names(x), subset.of = predict_types)
+
+  x$row_ids = row_ids
+  if (inherits(task, "TaskSupervised"))
+    x$truth = task$truth(row_ids)
+
+  pdata = new_prediction_data(x, task_type = task$task_type)
+  if (check) {
+    pdata = check_prediction_data(pdata)
+  }
+
+  pdata
+}
