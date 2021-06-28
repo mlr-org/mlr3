d4798f3c7133ee5e514085c36cfa9d619568d72a
Michel Lang <michellang@gmail.com>
2021-06-28 22:50:51 +0200

    fix last commit

--- R/helper.R
+++ R/helper.R
@@ -49,7 +49,7 @@ allow_partial_matching = list(
 # extract values from a single column of a data table
 # tries to avoid the overhead of data.table for small tables
 fget = function(tab, i, j, key) {
-  if (task$nrow < 1000L) {
+  if (nrow(tab) < 1000L) {
     tab[list(i), j, on = key, with = FALSE][[1L]]
   } else {
     x = tab[[key]]
