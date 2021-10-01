f9e1e281f0bc6f5b4dd30c68eebd89e50f52811c
Michel Lang <michellang@gmail.com>
2021-10-01 16:42:03 +0200

    improve cross-linking

--- man-roxygen/seealso_benchmark.R
+++ man-roxygen/seealso_benchmark.R
@@ -1,6 +1,8 @@
 #' @seealso
 #'
-#' Chapter in the [mlr3book](https://mlr3book.mlr-org.com/):
+#' * Chapter in the [mlr3book](https://mlr3book.mlr-org.com/):
 #' \url{https://mlr3book.mlr-org.com/basics.html#benchmarking}
+#' * Package \CRANpkg{mlr3viz} for some generic visualizations.
+#' * \CRANpkg{mlr3benchmark} for post-hoc analysis of benchmark results.
 #'
 #' @family benchmark
--- man-roxygen/seealso_learner.R
+++ man-roxygen/seealso_learner.R
@@ -7,6 +7,7 @@
 #' * [Dictionary][mlr3misc::Dictionary] of [Learners][Learner]: [mlr_learners]
 #' * `as.data.table(mlr_learners)` for a table of available [Learners][Learner] in the running session (depending on the loaded packages).
 #' * \CRANpkg{mlr3pipelines} to combine learners with pre- and postprocessing steps.
+#' * Package \CRANpkg{mlr3viz} for some generic visualizations.
 #' * Extension packages for additional task types:
 #'    * \CRANpkg{mlr3proba} for probabilistic supervised regression and survival analysis.
 #'    * \CRANpkg{mlr3cluster} for unsupervised clustering.
