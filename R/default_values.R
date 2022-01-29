f798c545ad8fc26d41e53b96ce8c71b11d1c8b7c
be-marc <marcbecker@posteo.de>
2022-01-29 16:50:22 +0100

    feat: add default_values function to extract default values from learners

---
+++ R/default_values.R
@@ -0,0 +1,33 @@
+#' @title Extract Hyperparameter Default Values
+#'
+#' @description
+#' Extract hyperparameter default values.
+#'
+#' @param x ([Learner])\cr
+#'   Learner to extract hyperparameter default values from.
+#' @param search_search ([paradox::ParamSet])\cr
+#'   Learner hyperparameter values are subsetted to hyperparameters in search space.
+#' @param task ([Task])\cr
+#'   Task to train the learner on.
+#'
+#' @return `list()`.
+#' @export
+default_values = function(x, search_space, task, ...) { # nolint
+  UseMethod("default_values")
+}
+
+#' @export
+#' @rdname default_values
+default_values.Learner = function(x, search_space, task, ...) { # nolint
+  x$param_set$default[search_space$ids()]
+}
+
+#' @export
+#' @rdname default_values
+default_values.LearnerClassifRpart = function(x, search_space, task, ...) { # nolint
+  special_defaults = list(
+    minbucket = 20 / 3
+  )
+  defaults = insert_named(x$param_set$default, special_defaults)
+  defaults[search_space$ids()]
+}
