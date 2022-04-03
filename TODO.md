df30b5a5e1c5e57b23a6ef8555ac64425116085d
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-03 11:41:11 +0200

    remove TODO and typo

--- TODO.md
+++
@@ -1,1 +0,0 @@
-task$row_roles$holdout = 1:10 should not work (?)
--- man-roxygen/param_learner_properties.R
+++ man-roxygen/param_learner_properties.R
@@ -7,7 +7,7 @@
 #'   * `"importance"`: The learner supports extraction of importance scores, i.e. comes with an `$importance()` extractor function (see section on optional extractors in [Learner]).
 #'   * `"selected_features"`: The learner supports extraction of the set of selected features, i.e. comes with a `$selected_features()` extractor function (see section on optional extractors in [Learner]).
 #'   * `"oob_error"`: The learner supports extraction of estimated out of bag error, i.e. comes with a `oob_error()` extractor function (see section on optional extractors in [Learner]).
-#'   * `validation`: The learner supports the use of validation data (e.g. for early stopping).
+#'   * `"validation"`: The learner supports the use of validation data (e.g. for early stopping).
 #'   For learners with this property the argument `valid_ids` can be set when calling the `$train`
 #'   method of the learner. During a `resample()` call the learner also has access to the validation
 #'   ids (in earlier versions this set was named test set).
