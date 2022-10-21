06a261e9d5e059552671883e3783b03e2836c848
be-marc <marcbecker@posteo.de>
2022-10-21 07:25:28 +0200

    chore: remove catn

--- R/helper_print.R
+++ R/helper_print.R
@@ -1,7 +1,3 @@
 # format_list_item = function(x, ...) {
 #   UseMethod("format_list_item")
 # }
-
-catn = function(..., file = "") {
-  cat(paste0(..., collapse = "\n"), "\n", sep = "", file = file)
-}
