cfa08ddc2218a706d793a968fb9172d96ed33db2
Michel Lang <michellang@gmail.com>
2023-04-05 14:13:42 +0200

    cleanup

--- R/benchmark_grid.R
+++ R/benchmark_grid.R
@@ -4,13 +4,21 @@
 #' Takes a lists of [Task], a list of [Learner] and a list of [Resampling] to
 #' generate a design in an [expand.grid()] fashion (a.k.a. cross join or Cartesian product).
 #'
-#' Resampling strategies are not allowed to be instantiated when passing the argument, and instead will be instantiated per task internally.
-#' The only exception to this rule applies if all tasks have exactly the same number of rows, and the resamplings are all instantiated for such tasks.
+#' There are two modes of operation, depending on the flag `paired`.
+#'
+#' * With `paired` set to `FALSE` (default), resampling strategies are not allowed to be instantiated, and instead will be instantiated per task internally.
+#'   The only exception to this rule applies if all tasks have exactly the same number of rows, and the resamplings are all instantiated for such tasks.
+#'   The grid will be generated based on the Cartesian product of tasks, learners, and resamplings.
+#' * With `paired` set to `TRUE`, tasks and resamplings are treated as pairs.
+#'   I.e., you must provide as many tasks as corresponding instantiated resamplings.
+#'   The grid will be generated based on the Cartesian product of learners and pairs.
 #'
 #' @param tasks (list of [Task]).
 #' @param learners (list of [Learner]).
 #' @param resamplings (list of [Resampling]).
-#' @param paired (logical) Set this to `TRUE` if the resamplings are instantiated on the tasks, i.e. the tasks and resamplings are paired.
+#' @param paired (`logical(1)`)\cr
+#'   Set this to `TRUE` if the resamplings are instantiated on the tasks, i.e., the tasks and resamplings are paired.
+#'   You need to provide the same number of tasks and instantiated resamplings.
 #'
 #' @return ([data.table::data.table()]) with the cross product of the input vectors.
 #'
@@ -55,13 +63,20 @@ benchmark_grid = function(tasks, learners, resamplings, paired = FALSE) {
   learners = assert_learners(as_learners(learners))
   resamplings = assert_resamplings(as_resamplings(resamplings))
 
-  if (paired) {
-    assert_true(length(tasks) == length(resamplings))
-    assert_true(all(map_lgl(resamplings, "is_instantiated")))
-    task_hashes = map_chr(tasks, "hash")
-    resampling_hashes = map_chr(resamplings, "task_hash")
-    if (!all(task_hashes == resampling_hashes)) {
-      stopf("If paired is `TRUE`, the resamplings must be instantiated on their task.")
+  if (assert_flag(paired)) {
+    if (length(tasks) != length(resamplings)) {
+      stopf("If `paired` is `TRUE`, you need to provide the same number of tasks and instantiated resamplings")
+    }
+
+    for (i in seq_along(tasks)) {
+      task = tasks[[i]]
+      resampling = resamplings[[i]]
+      if (!resamplings[[i]]$is_instantiated) {
+        stopf("Resampling #%i ('%s' for task '%s') is not instantiated", i, resampling$id, task$id)
+      }
+      if (resampling$task_hash != task$hash) {
+        stopf("Resampling #%i ('%s' for task '%s') is not instantiated on the corresponding task", i, resampling$id, task$id)
+      }
     }
 
     grid = CJ(task = seq_along(tasks), learner = seq_along(learners))
@@ -72,33 +87,31 @@ benchmark_grid = function(tasks, learners, resamplings, paired = FALSE) {
       learner = learners[grid$learner],
       resampling = resamplings[grid$task]
     )
-
-    set_data_table_class(tab, "benchmark_grid")
-    return(tab)
-  }
-
-  grid = CJ(task = seq_along(tasks), resampling = seq_along(resamplings))
-  is_instantiated = map_lgl(resamplings, "is_instantiated")
-
-  if (any(is_instantiated)) {
-    task_nrow = unique(map_int(tasks, "nrow"))
-    if (length(task_nrow) != 1L) {
-      stopf("All resamplings must be uninstantiated, or must operate on tasks with the same number of rows")
-    }
-    if (!identical(task_nrow, unique(map_int(resamplings, "task_nrow")))) {
-      stop("A Resampling is instantiated for a task with a different number of observations")
-    }
-    instances = pmap(grid, function(task, resampling) resamplings[[resampling]]$clone())
   } else {
-    instances = pmap(grid, function(task, resampling) resamplings[[resampling]]$clone()$instantiate(tasks[[task]]))
+    grid = CJ(task = seq_along(tasks), resampling = seq_along(resamplings))
+    is_instantiated = map_lgl(resamplings, "is_instantiated")
+
+    if (any(is_instantiated)) {
+      task_nrow = unique(map_int(tasks, "nrow"))
+      if (length(task_nrow) != 1L) {
+        stopf("All resamplings must be uninstantiated, or must operate on tasks with the same number of rows")
+      }
+      if (!identical(task_nrow, unique(map_int(resamplings, "task_nrow")))) {
+        stop("A Resampling is instantiated for a task with a different number of observations")
+      }
+      instances = pmap(grid, function(task, resampling) resamplings[[resampling]]$clone())
+    } else {
+      instances = pmap(grid, function(task, resampling) resamplings[[resampling]]$clone()$instantiate(tasks[[task]]))
+    }
+
+    grid$instance = seq_row(grid)
+    grid = grid[CJ(task = seq_along(tasks), learner = seq_along(learners)), on = "task", allow.cartesian = TRUE]
+
+    tab = data.table(task = tasks[grid$task], learner = learners[grid$learner], resampling = instances[grid$instance])
   }
 
-  grid$instance = seq_row(grid)
-  grid = grid[CJ(task = seq_along(tasks), learner = seq_along(learners)), on = "task", allow.cartesian = TRUE]
-
-  tab = data.table(task = tasks[grid$task], learner = learners[grid$learner], resampling = instances[grid$instance])
   set_data_table_class(tab, "benchmark_grid")
-  tab
+  return(tab)
 }
 
 #' @export
