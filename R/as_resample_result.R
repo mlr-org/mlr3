fd784dce82250d01ae0a78ba090cfe2211c299a3
Sebastian Fischer <sebf.fischer@gmail.com>
2023-04-28 20:16:17 +0200

    feat: add S3 method for ResultData object for as_resample_result (#920)

--- R/as_resample_result.R
+++ R/as_resample_result.R
@@ -26,6 +26,13 @@ as_resample_result.ResampleResult = function(x, ...) { # nolint
   x
 }
 
+#' @rdname as_resample_result
+#' @param view (`character()`)\cr
+#'   See construction argument `view` of [`ResampleResult`].
+#' @export
+as_resample_result.ResultData = function(x, view = NULL, ...) { # nolint
+  ResampleResult$new(x, view = view)
+}
 
 #' @rdname as_resample_result
 #' @inheritParams as_result_data
