07398100bc034ca51ec0dc72d169ce37de800721
Michel Lang <michellang@gmail.com>
2023-07-06 07:27:39 +0200

    docs: fix links to book

--- man-roxygen/seealso_prediction.R
+++ man-roxygen/seealso_prediction.R
@@ -1,7 +1,7 @@
 #' @seealso
 #'
 #' * Chapter in the [mlr3book](https://mlr3book.mlr-org.com/):
-#'   \url{https://mlr3book.mlr-org.com/basics.html#train-predict}
+#'   \url{https://mlr3book.mlr-org.com/chapters/chapter2/data_and_basic_modeling.html}
 #' * Package \CRANpkg{mlr3viz} for some generic visualizations.
 #' * Extension packages for additional task types:
 #'    * \CRANpkg{mlr3proba} for probabilistic supervised regression and survival analysis.
