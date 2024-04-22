ef59a0b612bb913c7ee130d3d56f5622c61665c7
Sebastian Fischer <sebf.fischer@gmail.com>
2024-04-22 14:32:01 +0200

    better docs

--- man-roxygen/param_unmarshal.R
+++ man-roxygen/param_unmarshal.R
@@ -2,4 +2,4 @@
 #'   Whether to unmarshal learners that were marshaled during the execution.
 #'   Setting this to `FALSE` does not guarantee that the learners are marshaled.
 #'   For example, with sequential execution and no encapsulation, marshaling is not necessary.
-#'   If you want all learners in marshaled form, you need to call `$marshal()` on the result object.
+#'   If you want to ensure that all learners are in marshaled form, you need to call `$marshal()` on the result object.
