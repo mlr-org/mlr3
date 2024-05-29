4b6518b9c046c87425edb71f3d1b2377baf8745a
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-29 10:52:42 +0200

    improve docs

--- man-roxygen/param_unmarshal.R
+++ man-roxygen/param_unmarshal.R
@@ -1,4 +1,4 @@
 #' @param unmarshal [`Learner`]\cr
 #'   Whether to unmarshal learners that were marshaled during the execution.
-#'   If `TRUE` all learners are in unmarshaled form.
-#'   If `FALSE`, all learners that need marshaling will be in marshaled form.
+#'   If `TRUE` all models are stored in unmarshaled form.
+#'   If `FALSE`, all learners (that need marshaling) are stored in marshaled form.
