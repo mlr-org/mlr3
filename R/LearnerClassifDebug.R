fe027e3652eb0b2ff7b049e4b308d87e4057c0b5
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-31 09:22:31 +0200

    better error message

--- R/LearnerClassifDebug.R
+++ R/LearnerClassifDebug.R
@@ -327,7 +327,7 @@ unmarshal_model.classif.debug_model_marshaled = function(model, inplace = FALSE,
 assert_internal_tuning.LearnerClassifDebug = function(learner, ids, ...) {
   if (length(ids)) {
     assert_true(isTRUE(learner$param_set$values$early_stopping), .var.name = "early stopping parameter")
-    assert_true(!is.null(learner$validate))
+    assert_false(is.null(learner$validate), .var.name = "validate parameter is null")
   }
   return(learner)
 }
