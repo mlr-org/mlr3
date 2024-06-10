381b045376d32d883e181fec3503ac92891c504e
john <bblodfon@gmail.com>
2024-06-10 13:11:22 +0200

    add "nfeatures" active field to Task

--- R/Task.R
+++ R/Task.R
@@ -886,6 +886,14 @@ Task = R6Class("Task",
       length(private$.col_roles$feature) + length(private$.col_roles$target)
     },
 
+    #' @field nfeatures (`integer(1)`)\cr
+    #' Returns the total number of columns with role "feature" (i.e. the
+    #' number of "active" features in the task).
+    nfeatures = function(rhs) {
+      assert_ro_binding(rhs)
+      length(private$.col_roles$feature)
+    },
+
     #' @field feature_types ([data.table::data.table()])\cr
     #' Returns a table with columns `id` and `type` where `id` are the column names of "active"
     #' features of the task and `type` is the storage type.
