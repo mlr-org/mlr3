930dd49e9ff88c5be9692defd00239eb6f3ad056
be-marc <marcbecker@posteo.de>
2024-08-23 15:57:39 +0200

    test fix

--- tests/testthat/test_mlr_reflections.R
+++ tests/testthat/test_mlr_reflections.R
@@ -6,7 +6,7 @@ on.exit({
 
 mlr_reflections$task_types = setkeyv(rbind(mlr_reflections$task_types, rowwise_table(
   ~type,  ~package, ~task,              ~learner,        ~prediction,         ~prediction_data,        ~measure,
-  "test", "mlr3",   "TaskClassifTest", "LearnerClassif", "PredictionClassif", "PredictionDataClassif", "MeasureClassif",
+  "test", "mlr3",   "TaskClassifTest", "LearnerClassif", "PredictionClassif", "PredictionDataClassif", "MeasureClassif"
 )), "type")
 
 mlr_reflections$task_col_roles$test = c(mlr_reflections$task_col_roles$classif, "test")
