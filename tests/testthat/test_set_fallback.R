65cc2c5c3a5946ecdcbdeab7adae9a8f4533ca5f
be-marc <marcbecker@posteo.de>
2024-08-30 11:07:50 +0200

    tests: set_fallback quantiles

--- tests/testthat/test_set_fallback.R
+++ tests/testthat/test_set_fallback.R
@@ -26,4 +26,16 @@ test_that("set_fallback() works", {
   expect_class(learner, "LearnerRegrDebug")
   expect_class(learner$fallback, "LearnerRegrFeatureless")
   expect_equal(learner$fallback$predict_type, "se")
+
+  learner = lrn("regr.debug",
+    predict_type = "quantiles",
+    quantiles = c(0.1, 0.9),
+    quantile_response = 0.1)
+  set_fallback(learner)
+
+  expect_class(learner, "LearnerRegrDebug")
+  expect_class(learner$fallback, "LearnerRegrFeatureless")
+  expect_equal(learner$fallback$predict_type, "quantiles")
+  expect_equal(learner$fallback$quantiles, c(0.1, 0.9))
+  expect_equal(learner$fallback$quantile_response, 0.1)
 })
