5ffcfeeb1879e756b0cbd26a60a75faaa46aab78
Marc Becker <33069354+be-marc@users.noreply.github.com>
2024-08-31 11:32:09 +0200

    feat: regr.featureless quantile prediction (#1125)
    * feat: regr.featuresless quantile prediction
    
    * docs: update
    
    * fix: pass quantiles to fallback
    
    * feat: set default fallback with set_fallback
    
    * chore: news
    
    * tests: set_fallback quantiles
    
    * refactor: switch to default_fallback
    
    * fix: arguments
    
    * chore: rename
    
    ---------
    
    Co-authored-by: Bernd Bischl <bernd_bischl@gmx.net>

--- tests/testthat/test_default_fallback.R
+++ tests/testthat/test_default_fallback.R
@@ -22,4 +22,15 @@ test_that("fallback = default_fallback() works", {
 
   expect_class(fallback, "LearnerRegrFeatureless")
   expect_equal(fallback$predict_type, "se")
+
+  learner = lrn("regr.debug",
+    predict_type = "quantiles",
+    quantiles = c(0.1, 0.9),
+    quantile_response = 0.1)
+  fallback = default_fallback(learner)
+
+  expect_class(fallback, "LearnerRegrFeatureless")
+  expect_equal(fallback$predict_type, "quantiles")
+  expect_equal(fallback$quantiles, c(0.1, 0.9))
+  expect_equal(fallback$quantile_response, 0.1)
 })
--- tests/testthat/test_mlr_learners_regr_featureless.R
+++ tests/testthat/test_mlr_learners_regr_featureless.R
@@ -12,3 +12,27 @@ test_that("regr.featureless works on featureless task", {
   expect_resample_result(rr)
   expect_number(rr$aggregate())
 })
+
+test_that("regr.featureless quantile prediction works", {
+  task = tsk("mtcars")
+
+  learner = lrn("regr.featureless",
+    predict_type = "quantiles",
+    quantiles = c(0.1, 0.5, 0.9),
+    quantile_response = 0.5)
+
+  learner$train(task)
+  expect_numeric(learner$model$quantiles, len = 3L)
+
+  pred = learner$predict(task)
+  expect_prediction(pred)
+  expect_subset("quantiles", pred$predict_types)
+  expect_matrix(pred$quantiles, ncols = 3L, nrows = task$nrow, any.missing = FALSE)
+  expect_names(colnames(pred$quantiles), identical.to = c("q0.1", "q0.5", "q0.9"))
+  expect_equal(pred$response, pred$quantiles[, 2L])
+
+  learner = lrn("regr.featureless",
+    predict_type = "quantiles")
+
+  expect_error(learner$train(task), "Quantiles")
+})
