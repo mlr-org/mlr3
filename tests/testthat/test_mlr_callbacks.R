288da828509fa079172228bd944bd8f6248836ec
be-marc <marcbecker@posteo.de>
2024-12-20 11:42:00 +0100

    ...

--- tests/testthat/test_mlr_callbacks.R
+++ tests/testthat/test_mlr_callbacks.R
@@ -1,25 +1,37 @@
-test_that("score_measure works", {
+test_that("model extractor works", {
   task = tsk("pima")
   learner = lrn("classif.rpart")
   resampling = rsmp("cv", folds = 3)
 
-  callback = clbk("mlr3.score_measures", measures = msr("selected_features"))
+  selected_features = function(learner) list(selected_features = learner$selected_features())
+  callback = clbk("mlr3.model_extractor", fun = selected_features)
 
   rr = resample(task, learner, resampling = resampling, callbacks = callback)
 
   expect_list(rr$data_extra)
   walk(rr$data_extra, function(data) {
-    expect_names(names(data), must.include = "score_measures")
-    expect_names(names(data[["score_measures"]]), must.include = "selected_features")
+    expect_names(names(data), must.include = "selected_features")
+    expect_subset(data[["selected_features"]], task$feature_names)
   })
+})
 
-  callback = clbk("mlr3.score_measures", measures = msrs(c("classif.ce", "selected_features")))
+test_that("holdout task works", {
+  task = tsk("pima")
+  task_holdout = task$clone()
+  learner = lrn("classif.rpart")
+  resampling = rsmp("cv", folds = 3)
+  splits = partition(task, 0.7)
+
+  task$filter(splits$train)
+  task_holdout$filter(splits$test)
+
+  callback = clbk("mlr3.holdout_task", task = task_holdout)
 
   rr = resample(task, learner, resampling = resampling, callbacks = callback)
 
   expect_list(rr$data_extra)
   walk(rr$data_extra, function(data) {
-    expect_names(names(data), must.include = "score_measures")
-    expect_names(names(data[["score_measures"]]), must.include = c("classif.ce", "selected_features"))
+    expect_names(names(data), must.include = "prediction_holdout")
+    expect_prediction(data[["prediction_holdout"]])
   })
 })
