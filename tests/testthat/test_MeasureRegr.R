7813a5629342529baf71827c827587162ed646f8
Lona <120468261+lona-k@users.noreply.github.com>
2025-05-12 13:50:20 +0200

    Fix: add parameter alpha to pinball measure (#1299)
    * fix: add alpha to pinball
    
    * add parameter for "sim.phi"
    
    * Add pinball as a distinct measure outside of MeasureSimple
    
    * adjust test

--- tests/testthat/test_MeasureRegr.R
+++ tests/testthat/test_MeasureRegr.R
@@ -7,9 +7,18 @@ test_that("Regression measures", {
 
   for (key in keys) {
     m = mlr_measures$get(key)
+
     if (is.na(m$task_type) || m$task_type == "regr") {
-      perf = m$score(prediction = p, task = task, learner = learner)
-      expect_number(perf, na.ok = FALSE, lower = m$range[1], upper = m$range[2])
+      if (m$predict_type == "quantiles") {
+        learner_q = lrn("regr.featureless", predict_type = "quantiles", quantiles = 0.5)
+        learner_q$train(task)
+        p_q = learner_q$predict(task)
+        perf = m$score(prediction = p_q, task = task, learner = learner_q)
+        expect_number(perf, na.ok = FALSE, lower = m$range[1], upper = m$range[2])
+      } else {
+        perf = m$score(prediction = p, task = task, learner = learner)
+        expect_number(perf, na.ok = FALSE, lower = m$range[1], upper = m$range[2])
+      }
     }
   }
 })
