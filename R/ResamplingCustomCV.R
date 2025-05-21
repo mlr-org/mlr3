c1d4273876626e2969d529b6559447ff243ccd48
Marc Becker <33069354+be-marc@users.noreply.github.com>
2025-05-21 09:08:43 +0200

    fix: add task_row_hash to custom cv (#1312)

--- R/ResamplingCustomCV.R
+++ R/ResamplingCustomCV.R
@@ -76,6 +76,7 @@ ResamplingCustomCV = R6Class("ResamplingCustomCV", inherit = Resampling,
       self$instance = split(task$row_ids, f, drop = TRUE)
       self$task_hash = task$hash
       self$task_nrow = task$nrow
+      self$task_row_hash = task$row_hash
       invisible(self)
     }
   ),
