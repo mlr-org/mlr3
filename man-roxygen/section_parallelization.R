f511627370226e81e24dea23c7d4deec24bcbcb5
be-marc <marcbecker@posteo.de>
2025-05-22 13:22:40 +0200

    ...

--- man-roxygen/section_parallelization.R
+++ man-roxygen/section_parallelization.R
@@ -1,8 +1,9 @@
 #' @section Parallelization:
 #'
-#' This function can be parallelized with the \CRANpkg{future} package.
-#' One job is one resampling iteration, and all jobs are send to an apply function
-#' from \CRANpkg{future.apply} in a single batch.
+#' This function can be parallelized with the \CRANpkg{future} or \CRANpkg{mirai} package.
+#' One job is one resampling iteration.
+#' All jobs are send to an apply function from \CRANpkg{future.apply} or `mirai::mirai_map()` in a single batch.
 #' To select a parallel backend, use [future::plan()].
+#' To use \CRANpkg{mirai}, call `mirai::daemons()` before calling this function.
 #' More on parallelization can be found in the book:
 #' \url{https://mlr3book.mlr-org.com/chapters/chapter10/advanced_technical_aspects_of_mlr3.html}
