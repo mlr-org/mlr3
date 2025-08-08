8c61cd8a2c1c3d64abbb1075597a326c4ac2de92
be-marc <marcbecker@posteo.de>
2025-08-08 13:11:08 +0200

    ...

--- R/helper_data_table.R
+++ R/helper_data_table.R
@@ -1,21 +1,22 @@
 # extract values from a single column of a data table
 # tries to avoid the overhead of data.table for small tables
 # returns a vector sorted by the order of i
-# use only when the values in key column are unique
+# if the key column is not unique, the first match is returned
+# if no-matching elements are found, NA is returned
 fget = function(tab, i, j, key) {
   if (nrow(tab) > 1000L) {
-    ijoin(tab, i, j, key)[[1L]]
+    ijoin(tab, i, j, key, mult = "first")[[1L]]
   } else {
     x = tab[[key]]
     tab[[j]][match(i, x)]
   }
 }
 
-ijoin = function(tab, .__i__, .__j__, .__key__) {
+ijoin = function(tab, .__i__, .__j__, .__key__, mult = "all") {
   if (!is.list(.__i__)) {
     .__i__ = list(.__i__)
   }
-  tab[.__i__, .__j__, with = FALSE, nomatch = NULL, on = .__key__]
+  tab[.__i__, .__j__, with = FALSE, nomatch = NA, on = .__key__, mult = mult]
 }
 
 # updating join:
--- tests/testthat/test_helper_data_table.R
+++ tests/testthat/test_helper_data_table.R
@@ -1,28 +1,50 @@
-test_that("fget returns values in order of i for small tables", {
-  tab = data.table(key_col = c("a", "b", "c"), val = c(1L, 2L, 3L))
-  res = fget(tab, i = c("b", "a"), j = "val", key = "key_col")
+test_that("fget returns values in order of i", {
+  # small table
+  tab = data.table(key_col = c("k0001", "k0002", "k0003"), val = c(1L, 2L, 3L))
+  res = fget(tab, i = c("k0002", "k0001"), j = "val", key = "key_col")
   expect_equal(res, c(2L, 1L))
+
+  # large table
+  n = 1200L
+  tab = data.table(key_col = sprintf("k%04d", 1:n), val = 1:n)
+  res = fget(tab, i = c("k1000", "k0201"), j = "val", key = "key_col")
+  expect_equal(res, c(1000L, 201L))
 })
 
+test_that("non-matching elements return NA", {
+  # small table
+  tab = data.table(key_col = c("k0001", "k0002", "k0003"), val = c(1L, 2L, 3L))
+  res = fget(tab, i = c("k0002", "k0004"), j = "val", key = "key_col")
+  expect_equal(res, c(2L, NA))
 
-test_that("fget repeats matches for small tables", {
-  tab = data.table(key_col = c("a", "b", "c"), val = c(1L, 2L, 3L))
-  res = fget(tab, i = c("b", "b", "c"), j = "val", key = "key_col")
+  # large table
+  n = 1200L
+  tab = data.table(key_col = sprintf("k%04d", 1:n), val = 1:n)
+  res = fget(tab, i = c("k1000", "k1201"), j = "val", key = "key_col")
+  expect_equal(res, c(1000L, NA))
+})
+
+test_that("fget repeats matches", {
+  # small table
+  tab = data.table(key_col = c("k0001", "k0002", "k0003"), val = c(1L, 2L, 3L))
+  res = fget(tab, i = c("k0002", "k0002", "k0003"), j = "val", key = "key_col")
   expect_equal(res, c(2L, 2L, 3L))
+
+  # large table
+  n = 1200L
+  tab = data.table(key_col = sprintf("k%04d", 1:n), val = 1:n)
+  res = fget(tab, i = tab$key_col[c(1000, 200, 700, 1000)], j = "val", key = "key_col")
+  expect_equal(res, c(1000L, 200L, 700L, 1000L))
 })
 
-test_that("fget returns values in order of i for large tables", {
-  n = 1200
-  tab = data.table(key_col = sprintf("k%04d", 1:n), val = 1:n)
-  i_vec = tab$key_col[c(1000, 200, 700)]
-  res = fget(tab, i = i_vec, j = "val", key = "key_col")
-  expect_equal(res, c(1000, 200, 700))
-})
+test_that("fget on non-unique key column", {
+  tab = data.table(key_col = c("k0001", "k0002", "k0002"), val = c(1L, 2L, 3L))
+  res = fget(tab, i = c("k0001", "k0002"), j = "val", key = "key_col")
+  expect_equal(res, c(1L, 2L))
 
-test_that("fget repeats matches for large tables", {
-  n = 1200
+  n = 1200L
   tab = data.table(key_col = sprintf("k%04d", 1:n), val = 1:n)
-  i_vec = tab$key_col[c(1000, 200, 700, 1000)]
-  res = fget(tab, i = i_vec, j = "val", key = "key_col")
-  expect_equal(res, c(1000, 200, 700, 1000))
+  tab[3, key_col := "k0002"]
+  res = fget(tab, i = c("k0001", "k0002"), j = "val", key = "key_col")
+  expect_equal(res, c(1L, 2L))
 })
