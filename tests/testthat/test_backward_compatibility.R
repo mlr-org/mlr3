4f6a6b1c098244e2ba61d432dae8b680d3d152d6
be-marc <marcbecker@posteo.de>
2025-08-11 15:00:29 +0200

    ...

--- tests/testthat/test_backward_compatibility.R
+++ tests/testthat/test_backward_compatibility.R
@@ -1,17 +1,18 @@
 skip_if(Sys.getenv("GITHUB_ACTIONS") != "true", "Not on GitHub Actions")
+github_path = Sys.getenv("GITHUB_WORKSPACE")
 
 test_that("task classif backward compatibility", {
-  task = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/task_classif.rds")
+  task = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "task_classif.rds"))
   expect_task(task)
 })
 
 test_that("task regr backward compatibility", {
-  task = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/task_regr.rds")
+  task = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "task_regr.rds"))
   expect_task(task)
 })
 
 test_that("learner classif backward compatibility", {
-  learner_classif = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/learner_classif.rds")
+  learner_classif = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "learner_classif.rds"))
   expect_learner(learner_classif)
 
   task = tsk("pima")
@@ -23,7 +24,7 @@ test_that("learner classif backward compatibility", {
 })
 
 test_that("learner regr backward compatibility", {
-  learner_regr = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/learner_regr.rds")
+  learner_regr = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "learner_regr.rds"))
   expect_learner(learner_regr)
 
   task = tsk("mtcars")
@@ -35,12 +36,12 @@ test_that("learner regr backward compatibility", {
 })
 
 test_that("resampling backward compatibility", {
-  resampling = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/resampling.rds")
+  resampling = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "resampling.rds"))
   expect_resampling(resampling)
 })
 
 test_that("resample result backward compatibility", {
-  rr = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/rr.rds")
+  rr = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "rr.rds"))
   expect_resample_result(rr)
 
   score = rr$score(msr("classif.ce"))
@@ -49,7 +50,7 @@ test_that("resample result backward compatibility", {
 })
 
 test_that("benchmark result backward compatibility", {
-  bmr = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/bmr.rds")
+  bmr = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "bmr.rds"))
   expect_benchmark_result(bmr)
 
   score = bmr$score(msr("classif.ce"))
@@ -62,7 +63,7 @@ test_that("benchmark result backward compatibility", {
 })
 
 test_that("measure backward compatibility", {
-  measure = readRDS("/home/runner/work/mlr3/mlr3/tests/testthat/_object_snapshots/measure.rds")
+  measure = readRDS(file.path(github_path, "mlr3", "tests", "testthat", "_object_snapshots", "measure.rds"))
   expect_measure(measure)
 
   task = tsk("pima")
