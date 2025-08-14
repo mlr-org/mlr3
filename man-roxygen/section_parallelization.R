8619e08fb5c21e0f1dca0f38985e4096513218a5
be-marc <marcbecker@posteo.de>
2025-08-14 09:14:21 +0200

    ...

--- man-roxygen/section_parallelization.R
+++ man-roxygen/section_parallelization.R
@@ -6,6 +6,6 @@
 #' To select a parallel backend, use [future::plan()].
 #' To use `mirai`, call `mirai::daemons()` before calling this function.
 #' The `future` package guarantees reproducible results independent of the parallel backend.
-#' The results of `mirai` will not be the same but can be made reproducible by setting a `seed` and `dispatcher = FALSE` when calling `mirai::daemons()`.
+#' The results of `mirai` will not be the same but can be made reproducible by setting a `seed` when calling `mirai::daemons()`.
 #' More on parallelization can be found in the book:
 #' \url{https://mlr3book.mlr-org.com/chapters/chapter10/advanced_technical_aspects_of_mlr3.html}
