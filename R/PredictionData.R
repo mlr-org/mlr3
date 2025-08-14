6e9ed76afcc651984874c27fc4eb8da5a4397059
mb706 <mlr.developer@mb706.com>
2025-08-14 13:02:24 +0200

    temp fix

--- R/PredictionData.R
+++ R/PredictionData.R
@@ -21,7 +21,7 @@
 NULL
 
 new_prediction_data = function(li, task_type) {
-  li[] = discard(li, is.null) # keep attributes by assigning to []
+  li = discard(li, is.null)
   class(li) = c(fget_key(mlr_reflections$task_types, task_type, "prediction_data", "type"), "PredictionData")
   li
 }
