4859d2b0a11a6e67249d4586f91e89b8faf6715d
Sebastian Fischer <sebf.fischer@gmail.com>
2025-08-15 18:30:54 +0200

    add mirai test

--- tests/testthat/test_Learner.R
+++ tests/testthat/test_Learner.R
@@ -899,6 +899,26 @@ test_that("error conditions are working: try", {
   expect_error(l$train(tsk("iris")), regexp = NA)
 })
 
+test_that("error conditions are working: mirai", {
+  l = lrn("classif.debug",
+    timeout = c(train = 0.01),
+    # Sys.sleep does not get interrupted reliably
+    sleep_train = function() while (TRUE) NULL
+  )
+
+  l$encapsulate(
+    "mirai",
+    lrn("classif.featureless"),
+    function(x) {
+      !inherits(x, "Mlr3ErrorTimeout")
+    }
+  )
+
+  expect_error(l$train(tsk("iris")), regexp = "reached elapsed time limit")
+  l$configure(error_train = 1, sleep_train = NULL, timeout = c(train = Inf, predict = Inf))
+  expect_error(l$train(tsk("iris")), regexp = NA)
+})
+
 test_that("oob_error is available without storing models via $.extract_oob_error()", {
   LearnerDummyOOB = R6::R6Class("LearnerDummyOOB", inherit = LearnerClassif,
     public = list(
