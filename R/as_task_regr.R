28de4c7a087757e24f50a3efebbe6c1c364530d1
Maximilian Mücke <muecke.maximilian@gmail.com>
2025-11-20 18:19:19 +0100

    perf: use `attr()` instead of `attributes()` for extracting single attribute (#1409)

--- R/as_task_regr.R
+++ R/as_task_regr.R
@@ -85,7 +85,7 @@ as_task_regr.formula = function(x, data, id = deparse1(substitute(data)), label 
   assert_data_frame(data)
   assert_subset(all.vars(x), c(names(data), "."), .var.name = "formula")
 
-  if (!attributes(terms(x, data = data))$response) {
+  if (!attr(terms(x, data = data), "response")) {
     stopf("Formula %s is missing a response", format(x))
   }
   tab = model.frame(x, data, na.action = "na.pass")
