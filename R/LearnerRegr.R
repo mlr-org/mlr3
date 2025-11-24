24022e07c329e95b7aebac697a45d6034c0e7b84
Marc Becker <33069354+be-marc@users.noreply.github.com>
2025-11-24 16:42:13 +0100

    Update R/LearnerRegr.R

--- R/LearnerRegr.R
+++ R/LearnerRegr.R
@@ -134,7 +134,7 @@ LearnerRegr = R6Class("LearnerRegr", inherit = Learner,
       }
 
       if ("quantiles" %nin% self$predict_types) {
-        error_learner_predict("Learner does not support predicting quantiles")  # TODO error_config/input?
+        error_learner_predict("Learner does not support predicting quantiles") 
       }
       private$.quantiles = assert_numeric(rhs, lower = 0, upper = 1, any.missing = FALSE, min.len = 1L, sorted = TRUE, .var.name = "quantiles")
 
