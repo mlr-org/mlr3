32c91be1302004b4da62b7efa6b53ed2a31ecfa5
Marc Becker <33069354+be-marc@users.noreply.github.com>
2026-01-23 07:25:59 +0100

    feat: disable printing of class ratios for large tasks (#1435)

--- tests/testthat/test_Task.R
+++ tests/testthat/test_Task.R
@@ -1030,3 +1030,13 @@ test_that("weights_measure + stratum works during resampling (#1405)", {
   task$set_col_roles("Species", roles = c("target", "stratum"))
   expect_resample_result(resample(task, lrn("classif.featureless"), rsmp("cv", folds = 3)))
 })
+
+test_that("class ratios are not printed for large tasks (#1382)", {
+  task = tsk("iris")
+  old_threshold = getOption("mlr3.print_class_ratio_threshold")
+  on.exit(options(mlr3.print_class_ratio_threshold = old_threshold))
+  expect_output(print(task), "setosa \\(33%\\), versicolor \\(33%\\), virginica \\(33%\\)")
+
+  options(mlr3.print_class_ratio_threshold = 0L)
+  expect_output(print(task), "setosa, versicolor, virginica", fixed = TRUE)
+})
