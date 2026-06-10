56d059c6b83d18e74980020ace061ed81e85dfcb
be-marc <marcbecker@posteo.de>
2026-06-10 15:02:22 +0200

    docs: spelling

--- man-roxygen/param_store_backends.R
+++ man-roxygen/param_store_backends.R
@@ -1,7 +1,7 @@
 #' @param store_backends (`logical(1)`)\cr
 #'   Keep the [DataBackend] of the [Task] in the [ResampleResult]?
 #'   Set to `TRUE` if your performance measures require a [Task],
-#'   or to analyse results more conveniently.
+#'   or to analyze results more conveniently.
 #'   Set to `FALSE` to reduce the file size and memory footprint
 #'   after serialization.
 #'   The current default is `TRUE`, but this eventually will be changed
